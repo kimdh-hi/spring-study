d00675d1891d1ad2a2ba17e3c547ec25a8c42ba5
daehyunKim <zbeld123@gmail.com>
2022-05-04 16:59:49 +0900

    init

---
+++ swagger/src/main/kotlin/com/study/swagger/config/SwaggerConfig.kt
@@ -0,0 +1,11 @@
+package com.study.swagger.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@Configuration
+class SwaggerConfig {
+    
+}
