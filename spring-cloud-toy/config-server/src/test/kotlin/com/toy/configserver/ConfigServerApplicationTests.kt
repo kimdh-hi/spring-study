6190d79fe8668c269e15381f2317d5192dce3b87
daehyunKim <zbeld123@gmail.com>
2022-05-25 22:57:12 +0900

    feat: spring cloud config test, chore: create spring cloud toy project

---
+++ spring-cloud-toy/config-server/src/test/kotlin/com/toy/configserver/ConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
