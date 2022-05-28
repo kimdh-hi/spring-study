1e690a2f09b603eb4b2f060d5e16128c3eca0a2c
daehyunKim <zbeld123@gmail.com>
2022-05-28 19:55:28 +0900

    gradle - task, dependsOn, finalizedBy, defaultTasks

--- gradle/gradle-basic/build.gradle.kts
+++ gradle/gradle-basic/build.gradle.kts
@@ -21,3 +21,24 @@ task("firstTask") {
     }
 }
 println("end")
+
+task("deployToProd") {
+    doLast {
+        println("complete deploy to prod.")
+    }
+}.dependsOn("deployToStage") // deployToProd 테스크가 실행될 때 deployToStage 가 선행된다.
+    .finalizedBy("cleanupFiles") // deployToProd 테스크의 실행이 완료되고 cleanupFiles 가 실행된다.
+
+task("deployToStage") {
+    doLast {
+        println("complete deploy to stage.")
+    }
+}
+
+task("cleanupFiles") {
+    doLast {
+        println("complete cleanup files.")
+    }
+}
+
+defaultTasks("deployToProd") // task 이름없이 gradle 로 실행시 기본으로 실행되는 task 지정
