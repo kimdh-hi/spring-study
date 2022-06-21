3128789de97da2532057dff8c9d5a61be3d54278
daehyunKim <zbeld123@gmail.com>
2022-06-21 19:03:26 +0900

    feat(spring-thymeleaf): 개행처리

---
+++ spring-thymeleaf/src/test/kotlin/com/toy/springthymeleaf/SpringThymeleafApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springthymeleaf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringThymeleafApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
