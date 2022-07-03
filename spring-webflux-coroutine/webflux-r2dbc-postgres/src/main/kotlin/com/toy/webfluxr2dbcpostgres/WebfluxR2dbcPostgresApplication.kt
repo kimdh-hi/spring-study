619efb6773a78b05d57711fb21213f8ceaa027bd
kimdh-hi <zbeld123@gmail.com>
2022-07-03 22:21:30 +0900

    chore(webflux-r2dbc): init project

---
+++ spring-webflux-coroutine/webflux-r2dbc-postgres/src/main/kotlin/com/toy/webfluxr2dbcpostgres/WebfluxR2dbcPostgresApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.webfluxr2dbcpostgres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxR2dbcPostgresApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxR2dbcPostgresApplication>(*args)
+}
