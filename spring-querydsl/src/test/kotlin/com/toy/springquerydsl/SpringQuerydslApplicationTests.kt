67e5c5d347c876fdec073e46530961dae65c1f04
daehyunKim <zbeld123@gmail.com>
2022-07-13 21:15:12 +0900

    chore(querydsl): init querydsl sample project

---
+++ spring-querydsl/src/test/kotlin/com/toy/springquerydsl/SpringQuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springquerydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringQuerydslApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
