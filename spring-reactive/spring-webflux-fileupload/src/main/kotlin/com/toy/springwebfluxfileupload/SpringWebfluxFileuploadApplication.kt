f193c6c4b14a7ce999de86525ef3fdb09af8aba4
daehyunKim <zbeld123@gmail.com>
2022-07-19 19:54:14 +0900

    chore(webflux-file-upload): init

---
+++ spring-reactive/spring-webflux-fileupload/src/main/kotlin/com/toy/springwebfluxfileupload/SpringWebfluxFileuploadApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springwebfluxfileupload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxFileuploadApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxFileuploadApplication>(*args)
+}
---
+++ spring-reactive/spring-webflux-fileupload/src/test/kotlin/com/toy/springwebfluxfileupload/SpringWebfluxFileuploadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springwebfluxfileupload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxFileuploadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
