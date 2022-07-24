2052d9ae88ecf1cc2bf0028ead129bd9de70e122
daehyunKim <zbeld123@gmail.com>
2022-07-24 13:19:47 +0900

    feat(reactor-basic): flux.generate

--- spring-reactive/reactive-basic/src/main/kotlin/02-flux/07-Flux-generate.kt
+++ spring-reactive/reactive-basic/src/main/kotlin/02-flux/07-Flux-generate.kt
@@ -1,4 +1,16 @@
 package `02-flux`
 
-class `07-Flux-generate` {
+import `00-base`.ConsumerUtils
+import `00-base`.FakerUtils
+import reactor.core.publisher.Flux
+
+fun main() {
+  // 데이터를 무한하게 emit 한다.
+  Flux.generate {
+    val name = FakerUtils.FAKER.country().name()
+    it.next(name)
+    if (name.lowercase().contains("korea"))
+      it.complete()
+    // it.next(FakerUtils.FAKER.country().name()) // generate 는 한 개 flux instance? 만 사용되야 한다.
+  }.subscribe(ConsumerUtils.subscriber())
 }
--- spring-reactive/reactive-basic/src/main/kotlin/02-flux/08-Flux-generate-counter.kt
+++ spring-reactive/reactive-basic/src/main/kotlin/02-flux/08-Flux-generate-counter.kt
@@ -1,4 +1,23 @@
 package `02-flux`
 
-class `08-Flux-generate-counter` {
+import `00-base`.ConsumerUtils
+import `00-base`.FakerUtils
+import reactor.core.publisher.Flux
+
+// korea 를 포함하는 국가가 5개 나올 때까지 emit
+// counter 필요
+fun main() {
+  Flux.generate(
+    { 1 }, // counter 초기값
+    { counter, sink ->
+      val name = FakerUtils.FAKER.country().name()
+      sink.next(name)
+      if (name.lowercase().contains("korea")) {
+        if(counter < 5) return@generate counter+1 // counter 증가
+        sink.complete()
+      }
+      counter
+    }
+  )
+    .subscribe(ConsumerUtils.subscriber())
 }
