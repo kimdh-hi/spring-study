e1b496c3fe8d96e3a5e931e2e717474d941b0302
daehyunKim <zbeld123@gmail.com>
2022-07-26 16:17:55 +0900

    docs(conditionalOn): add comments

--- spring-reactive/spring-conditional/src/main/kotlin/com/toy/springconditional/config/EhcacheConfig.kt
+++ spring-reactive/spring-conditional/src/main/kotlin/com/toy/springconditional/config/EhcacheConfig.kt
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/*
+cache.ehcache 가 true 인 경우 동작하는 Configuration
+ */
 @Configuration
 @ConditionalOnProperty(value = ["cache.ehcache"], havingValue = true.toString())
 class EhcacheConfig: AbstractCacheConfig() {
--- spring-reactive/spring-conditional/src/main/kotlin/com/toy/springconditional/config/RedisConfig.kt
+++ spring-reactive/spring-conditional/src/main/kotlin/com/toy/springconditional/config/RedisConfig.kt
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/*
+cache.redis 가 true 인 경우 동작하는 Configuration
+ */
 @Configuration
 @ConditionalOnProperty(value = ["cache.redis"], havingValue = true.toString())
 class RedisConfig: AbstractCacheConfig() {
