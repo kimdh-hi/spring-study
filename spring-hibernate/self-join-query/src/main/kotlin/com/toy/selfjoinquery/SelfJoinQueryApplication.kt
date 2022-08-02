612940dc57dccdd772cf09770a5b595228a0b4fa
daehyunKim <zbeld123@gmail.com>
2022-08-02 21:12:49 +0900

    chore(self-join): self-join performance test sample project ...

---
+++ spring-hibernate/self-join-query/src/main/kotlin/com/toy/selfjoinquery/SelfJoinQueryApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.selfjoinquery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelfJoinQueryApplication
+
+fun main(args: Array<String>) {
+  runApplication<SelfJoinQueryApplication>(*args)
+}
