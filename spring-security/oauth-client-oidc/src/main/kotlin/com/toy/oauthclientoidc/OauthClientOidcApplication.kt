d3664a6975bbcea9b6af221e54ace4ccf12e63ad
daehyunKim <zbeld123@gmail.com>
2022-08-18 13:04:01 +0900

    chore(oidc): init

---
+++ spring-security/oauth-client-oidc/src/main/kotlin/com/toy/oauthclientoidc/OauthClientOidcApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.oauthclientoidc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthClientOidcApplication
+
+fun main(args: Array<String>) {
+  runApplication<OauthClientOidcApplication>(*args)
+}
---
+++ spring-security/oauth-client-oidc/src/test/kotlin/com/toy/oauthclientoidc/OauthClientOidcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.oauthclientoidc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthClientOidcApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
