5c868d7522a06be2b600d7c1bd7b51e620cf79a9
daehyunKim <zbeld123@gmail.com>
2022-08-19 20:56:11 +0900

    chore(oauth-basic): init

---
+++ spring-security/oauth-basic/src/main/kotlin/com/toy/oauthbasic/OauthBasicApplication.kt
@@ -0,0 +1,12 @@
+package com.toy.oauthbasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthBasicApplication
+
+fun main(args: Array<String>) {
+
+  runApplication<OauthBasicApplication>(*args)
+}
---
+++ spring-security/oauth-basic/src/test/kotlin/com/toy/oauthbasic/OauthBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.oauthbasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthBasicApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
