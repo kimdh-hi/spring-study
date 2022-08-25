7ff0bb9bed5379e70167bf12e1d7c23f8bf23c67
daehyunKim <zbeld123@gmail.com>
2022-08-25 21:43:06 +0900

    feat(rabbitmq): rabbitmq event-driven-architecture sample project init

---
+++ spring-jms/rabbitmq/rabbitmq-event-driven-architecture/email-service/src/main/kotlin/com/toy/emailservice/EmailServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.emailservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmailServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<EmailServiceApplication>(*args)
+}
---
+++ spring-jms/rabbitmq/rabbitmq-event-driven-architecture/email-service/src/test/kotlin/com/toy/emailservice/EmailServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.emailservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
