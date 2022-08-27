45bf49e98aeaac0415fd76ab69dfbec633c02a6a
daehyunKim <zbeld123@gmail.com>
2022-08-27 20:04:03 +0900

    feat(okhttp3): okHttpClientConfig

---
+++ spring-web-client/okhttp3/src/test/kotlin/com/toy/okhttp3/Okhttp3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.okhttp3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Okhttp3ApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
