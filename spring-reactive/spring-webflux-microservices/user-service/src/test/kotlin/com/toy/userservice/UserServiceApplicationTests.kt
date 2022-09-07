3d1292e6a42d976b58369cc904aa4cab476ac08b
daehyunKim <zbeld123@gmail.com>
2022-09-07 18:47:30 +0900

    chore(webflux-microservice): userservice init

---
+++ spring-reactive/spring-webflux-microservices/user-service/src/test/kotlin/com/toy/userservice/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.userservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
