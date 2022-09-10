6a1c1850abfdf23cf1434cb5f02a235f11ad7451
daehyunKim <zbeld123@gmail.com>
2022-09-10 11:53:13 +0900

    feat(redisson): ttl

--- spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/01-KeyValue.kt
+++ spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/01-KeyValue.kt
@@ -2,14 +2,17 @@ package com.toy.redissonbasic
 
 import com.toy.redissonbasic.base.BaseTest
 import org.junit.jupiter.api.Test
+import org.redisson.client.codec.StringCodec
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 class `01-KeyValue`: BaseTest() {
 
   @Test
   fun `key-value`() {
-    val bucket = client!!.getBucket<String>("test-key")
+    val bucket = client!!.getBucket<String>("test-key", StringCodec.INSTANCE)
     val set: Mono<Void> = bucket.set("test-value")
 
     val get: Mono<Void> = bucket.get()
@@ -17,6 +20,35 @@ class `01-KeyValue`: BaseTest() {
       .then()
 
     StepVerifier.create(set.concatWith(get))
+      .verifyComplete()
+  }
+
+  @Test
+  fun `ttl 설정`() {
+    val bucket = client!!.getBucket<String>("ttl-test-key", StringCodec.INSTANCE)
+    //ttl 설정
+    val set = bucket.set("test-value", 10, TimeUnit.SECONDS)
+
+    val get = bucket.get()
+      .doOnNext { println(it) }
+      .then()
+
+    StepVerifier.create(set.concatWith(get))
+      .verifyComplete()
+
+    Thread.sleep(5000)
+
+    // ttl 연장
+    val extendsResult = bucket.expire(Duration.ofSeconds(30))
+    StepVerifier.create(extendsResult)
+      .expectNext(true)
+      .verifyComplete()
+
+    // ttl 확인
+    val ttl = bucket.remainTimeToLive()
+      .doOnNext { println(it) }
+      .then()
+    StepVerifier.create(ttl)
       .verifyComplete()
   }
 }
