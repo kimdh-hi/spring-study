8abd36905ed4076b7e2ac62f4ac40ef272bd4e94
daehyunKim <zbeld123@gmail.com>
2022-09-11 13:09:47 +0900

    feat(redisson): eventListener (expired, deleted)

--- spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/05-EventListener.kt
+++ spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/05-EventListener.kt
@@ -2,15 +2,16 @@ package com.toy.redissonbasic
 
 import com.toy.redissonbasic.base.BaseTest
 import org.junit.jupiter.api.Test
+import org.redisson.api.DeletedObjectListener
 import org.redisson.api.ExpiredObjectListener
 import org.redisson.client.codec.StringCodec
 import reactor.test.StepVerifier
 import java.util.concurrent.TimeUnit
 
-class `05-Expire-Listener`: BaseTest() {
+class `05-EventListener`: BaseTest() {
 
   @Test
-  fun `ExpiredListener`() {
+  fun `expireEventListener`() {
     val bucket = client!!.getBucket<String>("user:1:username", StringCodec.INSTANCE)
     val setMono = bucket.set("test", 3, TimeUnit.SECONDS)
     val getMono = bucket.get()
@@ -20,7 +21,7 @@ class `05-Expire-Listener`: BaseTest() {
     // config set notify-keyspace-events AKE (redis-cli)
     val listenerMono = bucket.addListener(object: ExpiredObjectListener {
       override fun onExpired(name: String) {
-        println("expired name: $name")
+        println("onExpired: $name")
       }
     }).then()
 
@@ -28,5 +29,23 @@ class `05-Expire-Listener`: BaseTest() {
       .verifyComplete()
 
     Thread.sleep(5000)
+  }
+
+  @Test
+  fun `deleteEventListener`() {
+    val bucket = client!!.getBucket<String>("user:1:username")
+    val setMono = bucket.set("test")
+    // cli 로 delete 후 이벤트 동작 확인
+    // del user:1:username
+    val listenerMono = bucket.addListener(object: DeletedObjectListener {
+      override fun onDeleted(name: String) {
+        println("onDeleted: $name")
+      }
+    }).then()
+
+    StepVerifier.create(setMono.concatWith(listenerMono))
+      .verifyComplete()
+
+    Thread.sleep(100000L)
   }
 }
