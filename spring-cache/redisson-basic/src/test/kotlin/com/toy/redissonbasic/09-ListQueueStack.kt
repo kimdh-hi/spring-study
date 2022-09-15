86a749a728626aa9ffb9b7305cf0008f18b62df3
daehyunKim <zbeld123@gmail.com>
2022-09-15 18:23:43 +0900

    feat(redisson): list, queue, stack(deque.pollLast)

--- spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/09-ListQueueStack.kt
+++ spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/09-ListQueueStack.kt
@@ -5,18 +5,24 @@ import org.junit.jupiter.api.Test
 import org.redisson.client.codec.LongCodec
 import reactor.core.publisher.Flux
 import reactor.test.StepVerifier
+import java.util.stream.Collectors
+import java.util.stream.LongStream
 
 class `09-ListQueueStack`: BaseTest() {
 
   @Test
   fun test() {
     val list = client!!.getList<Long>("test-list", LongCodec.INSTANCE)
-    val listAdd = Flux.range(1, 10)
-      .map { it.toLong() }
-      .flatMap { list.add(it) }
-      .then()
+//    val longList = Flux.range(1, 10)
+//      .map { it.toLong() }
+//      .flatMap { list.add(it) }
+//      .then()
 
-    StepVerifier.create(listAdd)
+    val longList = LongStream.rangeClosed(1, 10)
+      .boxed()
+      .collect(Collectors.toList())
+
+    StepVerifier.create(list.addAll(longList))
       .verifyComplete()
     StepVerifier.create(list.size())
       .expectNext(10)
