4dcfa408438976f66ebb23dbbb359d1661d39b10
daehyunKim <zbeld123@gmail.com>
2022-09-16 19:28:02 +0900

    feat(redisson): simple pub-sub chat using pattern

--- spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/11-PubSub.kt
+++ spring-cache/redisson-basic/src/test/kotlin/com/toy/redissonbasic/11-PubSub.kt
@@ -2,6 +2,7 @@ package com.toy.redissonbasic
 
 import com.toy.redissonbasic.base.BaseTest
 import org.junit.jupiter.api.Test
+import org.redisson.api.RPatternTopicReactive
 import org.redisson.api.RTopicReactive
 import org.redisson.client.codec.StringCodec
 
@@ -11,6 +12,7 @@ class `11-PubSub`: BaseTest() {
    * [ publisher (redis-cli) ]
    * publish [channel-name] [message]
    * public test-room test-message
+   * 구독중인 sub 의 수를 응답
    */
 
   @Test
@@ -31,6 +33,16 @@ class `11-PubSub`: BaseTest() {
       .doOnError { println(it) }
       .doOnNext { println(it) }
       .subscribe()
+
+    Thread.sleep(600_000)
+  }
+
+  @Test
+  fun `subscriber3 - patternTopic`() {
+    val topic: RPatternTopicReactive = client!!.getPatternTopic("test-room*", StringCodec.INSTANCE)
+    topic.addListener(String::class.java) { pattern: CharSequence, topic: CharSequence, message: String ->
+      println("$pattern:$topic => $message")
+    }.subscribe()
 
     Thread.sleep(600_000)
   }
