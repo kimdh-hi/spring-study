04980c9dd55f6cea44bbfbfabe8a77439f82a6ad
daehyunKim <zbeld123@gmail.com>
2022-09-18 00:36:07 +0900

    feat(batch): flatFileJob itemProcessor(ItemProcessorAdapter),  itemWriter

--- lecture/fastcampus-spring-batch/src/main/kotlin/com/lecture/springbatchbasic/job/03-FlatFileJobConfig.kt
+++ lecture/fastcampus-spring-batch/src/main/kotlin/com/lecture/springbatchbasic/job/03-FlatFileJobConfig.kt
@@ -11,14 +11,20 @@ import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.launch.support.RunIdIncrementer
-import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.adapter.ItemProcessorAdapter
 import org.springframework.batch.item.file.FlatFileItemReader
+import org.springframework.batch.item.file.FlatFileItemWriter
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.FileSystemResource
+import java.io.File
 
 @Configuration
 class FlatFileJobConfig(
@@ -36,13 +42,49 @@ class FlatFileJobConfig(
 
   @JobScope
   @Bean
-  fun flatFileStep(itemReader: FlatFileItemReader<Member1VO>, memberService: MemberService): Step =
+  fun flatFileStep(
+    itemReader: FlatFileItemReader<Member1VO>,
+    memberItemProcessorAdapter: ItemProcessorAdapter<Member1VO, Member2VO>,
+    memberItemWriter: ItemWriter<Member2VO>
+  ): Step =
     sbf.get("flatFileStep")
       .chunk<Member1VO, Member2VO>(5)
       .reader(itemReader)
-      .processor(ItemProcessor<Member1VO, Member2VO> { memberService.classificationByAge(it) })
-      .writer { it.forEach { member -> println(member) } }
+//      .processor(ItemProcessor<Member1VO, Member2VO> { memberService.classificationByAge(it) })
+      .processor(memberItemProcessorAdapter)
+      .writer(memberItemWriter)
       .build()
+
+  @StepScope
+  @Bean
+  fun memberItemWriter(): FlatFileItemWriter<Member2VO> {
+    val fieldExtractor = BeanWrapperFieldExtractor<Member2VO>()
+    fieldExtractor.setNames(arrayOf("id", "name", "age", "type"))
+    fieldExtractor.afterPropertiesSet()
+
+    val lineAggregator = DelimitedLineAggregator<Member2VO>()
+    lineAggregator.setDelimiter("\t")
+    lineAggregator.setFieldExtractor(fieldExtractor)
+
+    File("processed-members.csv").createNewFile()
+    val resource = FileSystemResource("processed-members.csv")
+
+    return FlatFileItemWriterBuilder<Member2VO>()
+      .name("memberFlatFileItemWriter")
+      .resource(resource)
+      .lineAggregator(lineAggregator)
+      .build()
+  }
+
+  // ItemProcessorAdapter: processor 에서 service 로직 호출시 권장하는 방식
+  @StepScope
+  @Bean
+  fun memberItemProcessorAdapter(memberService: MemberService): ItemProcessorAdapter<Member1VO, Member2VO> {
+    val itemProcessorAdapter = ItemProcessorAdapter<Member1VO, Member2VO>()
+    itemProcessorAdapter.setTargetObject(memberService)
+    itemProcessorAdapter.setTargetMethod("classificationByAge")
+    return itemProcessorAdapter
+  }
 
   @StepScope
   @Bean
@@ -52,7 +94,7 @@ class FlatFileJobConfig(
       .lineTokenizer(DelimitedLineTokenizer())
       .linesToSkip(1)
       .fieldSetMapper(MemberFieldSetMapper())
-      .resource(ClassPathResource("books.csv"))
+      .resource(ClassPathResource("members.csv"))
       .build()
 
 }
