5b7967b71a070868e57bce00aea27cc905da1c1a
daehyunKim <zbeld123@gmail.com>
2022-09-20 20:18:20 +0900

    feat(fp): stream

---
+++ functional/src/main/java/ch2_Stream/Grouping_by.java
@@ -0,0 +1,26 @@
+package ch2_Stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Grouping_by {
+  public static void main(String[] args) {
+    // 3으로 나눈 나머지는 key로 한다
+    List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    Map<Integer, List<Integer>> groupingMap = list.stream()
+      .collect(Collectors.groupingBy(x -> x % 3));
+    System.out.println(groupingMap);
+
+    List<Integer> list2 = List.of(123, 234, 634, 140, 299);
+    Map<Integer, List<Integer>> groupingMap2 = list2.stream()
+      .collect(Collectors.groupingBy(x -> x % 10));
+    System.out.println(groupingMap2);
+
+    List<Integer> list3 = List.of(123, 234, 634, 140, 299);
+    Map<Integer, Set<Integer>> groupingMap3 = list3.stream()
+      .collect(Collectors.groupingBy(x -> x % 10, Collectors.toSet()));
+    System.out.println(groupingMap3);
+  }
+}
