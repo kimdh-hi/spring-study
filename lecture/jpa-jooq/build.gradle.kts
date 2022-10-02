c13889b5bb29471d689159483eb7f6c64fb2bedd
daehyunKim <zbeld123@gmail.com>
2022-10-02 15:18:45 +0900

    feat(jooq): setting

--- lecture/jpa-jooq/build.gradle.kts
+++ lecture/jpa-jooq/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   runtimeOnly("com.h2database:h2")
   jooqGenerator("com.h2database:h2")
+  jooqGenerator("jakarta.xml.bind:jakarta.xml.bind-api:3.0.1")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -47,4 +48,42 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
   useJUnitPlatform()
+}
+
+jooq {
+  version.set("3.16.4")
+  edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+  configurations {
+    create("main") {
+      generateSchemaSourceOnCompilation.set(true)
+
+      jooqConfiguration.apply {
+        jdbc.apply {
+          driver = "org.h2.Driver"
+          url = "jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE;"
+          user = "sa"
+        }
+
+        generator.apply {
+          name = "org.jooq.codegen.KotlinGenerator"
+
+          database.apply {
+            name = "org.jooq.meta.h2.H2Database"
+            inputSchema = "public"
+          }
+          generate.apply {
+            isDeprecated = false
+            isRecords = true
+            isImmutablePojos = true
+            isFluentSetters = true
+          }
+          target.apply {
+            packageName = "com.tr.domain.generated"
+          }
+          strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+        }
+      }
+    }
+  }
 }
--- lecture/jpa-jooq/src/main/kotlin/com/toy/jpajooq/repository/UserRepository.kt
+++ lecture/jpa-jooq/src/main/kotlin/com/toy/jpajooq/repository/UserRepository.kt
@@ -1,7 +1,7 @@
 package com.toy.jpajooq.repository
 
 import com.toy.jpajooq.domain.User
+import org.jooq.DSLContext
 import org.springframework.data.repository.CrudRepository
 
-interface UserRepository: CrudRepository<User, String> {
-}
+interface UserRepository: CrudRepository<User, String>
