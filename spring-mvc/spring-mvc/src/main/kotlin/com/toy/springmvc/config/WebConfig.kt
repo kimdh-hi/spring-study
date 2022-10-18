31031cda1a16595cbc77c6d088f72adae5a36009
daehyunKim <zbeld123@gmail.com>
2022-10-18 19:36:15 +0900

    feat(mvc): resourceHandler custom

--- spring-mvc/spring-mvc/src/main/kotlin/com/toy/springmvc/config/WebConfig.kt
+++ spring-mvc/spring-mvc/src/main/kotlin/com/toy/springmvc/config/WebConfig.kt
@@ -3,9 +3,11 @@ package com.toy.springmvc.config
 import com.toy.springmvc.interceptors.AnotherSampleInterceptor
 import com.toy.springmvc.interceptors.SampleInterceptor
 import org.springframework.context.annotation.Configuration
-import org.springframework.format.FormatterRegistry
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class WebConfig: WebMvcConfigurer {
@@ -27,5 +29,14 @@ class WebConfig: WebMvcConfigurer {
     registry.addInterceptor(AnotherSampleInterceptor())
       .addPathPatterns("/sample/**")
       .order(-1)
+  }
+
+  /**
+   * /test-resource 이하 리소스 요청은 classpath root 의 test-resource 이하에서 찾는다.
+   */
+  override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+    registry.addResourceHandler("/test-resource/**")
+      .addResourceLocations("classpath:/test-resource/")
+      .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
   }
 }
