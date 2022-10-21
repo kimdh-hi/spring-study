eff3c60ed757261f350e73c44c3d6fd7a449f220
daehyunKim <zbeld123@gmail.com>
2022-10-21 13:21:31 +0900

    feat(io): input/output stream

---
+++ java-io/java-io/src/main/kotlin/com/toy/02-io-stream/01-FileStream.kt
@@ -0,0 +1,17 @@
+package com.toy.`02-io-stream`
+
+import java.io.FileInputStream
+
+fun main() {
+
+  val fis = FileInputStream("/Users/daehyunkim/Documents/study/java-io/dir1/test1.txt")
+
+  while (true) {
+    val readByte = fis.read()
+    if(readByte == -1)
+      break
+    print(readByte)
+  }
+
+  fis.close()
+}
