014a1ef42de981e98a10d2bb41673f4db8492ba4
daehyunKim <zbeld123@gmail.com>
2022-10-23 21:54:56 +0900

    feat(nio): nio read/write

---
+++ java-io/java-io/src/main/kotlin/com/toy/05-nio/01-read-write.kt
@@ -0,0 +1,40 @@
+package com.toy.`05-nio`
+
+import java.io.File
+import java.nio.BufferUnderflowException
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+
+fun main() {
+  val byteBuffer = ByteBuffer.allocate(1024) // 1024kb
+  byteBuffer.putInt(123) // 4
+  byteBuffer.putInt(456) // 4
+  println(byteBuffer.position()) // 4 + 4 = 8
+  println(byteBuffer.limit()) // 1024
+
+  byteBuffer.flip()
+  println(byteBuffer.position()) // 4 + 4 = 8
+  println(byteBuffer.limit()) // 0
+
+  val path = Paths.get("src/main/kotlin/com/toy/05-nio/test.bin")
+  val fileChannel = FileChannel.open(path, StandardOpenOption.CREATE, StandardOpenOption.WRITE)
+  fileChannel.write(byteBuffer)
+  fileChannel.close()
+
+  val readFileChannel = FileChannel.open(path, StandardOpenOption.READ)
+  byteBuffer.clear()
+  readFileChannel.read(byteBuffer)
+  readFileChannel.close()
+  byteBuffer.flip()
+  val intBuffer = byteBuffer.asIntBuffer()
+
+    while(true) {
+      try {
+        println(intBuffer.get())
+      } catch (e: BufferUnderflowException) {
+        break
+      }
+    }
+}
