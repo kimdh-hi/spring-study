2dec367e9960517c7b3b00acd34cb849d2f31053
daehyunKim <zbeld123@gmail.com>
2022-11-10 20:57:36 +0900

    chore(batch): batch toy proejct init

---
+++ lecture/fs-batch-pt/pass-batch/src/main/kotlin/com/lecture/passbatch/PassBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.passbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PassBatchApplication
+
+fun main(args: Array<String>) {
+  runApplication<PassBatchApplication>(*args)
+}
---
+++ lecture/fs-batch-pt/pass-batch/src/test/kotlin/com/lecture/passbatch/PassBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.passbatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
