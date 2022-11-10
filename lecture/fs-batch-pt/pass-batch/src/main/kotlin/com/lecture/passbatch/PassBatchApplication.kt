5740c09a67f7d5ac8cba211521416219509943eb
daehyunKim <zbeld123@gmail.com>
2022-11-10 22:20:29 +0900

    feat(batch): setup mysql, init data

--- lecture/fs-batch-pt/pass-batch/src/main/kotlin/com/lecture/passbatch/PassBatchApplication.kt
+++ lecture/fs-batch-pt/pass-batch/src/main/kotlin/com/lecture/passbatch/PassBatchApplication.kt
@@ -1,14 +1,8 @@
 package com.lecture.passbatch
 
-import org.springframework.batch.core.Job
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.step.tasklet.TaskletStep
-import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableBatchProcessing
