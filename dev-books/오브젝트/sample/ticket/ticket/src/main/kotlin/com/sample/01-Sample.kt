9108b0d4bd7fc096a16ee971cd1231bfbf80d74d
daehyunKim <zbeld123@gmail.com>
2022-11-15 20:52:26 +0900

    feat(object) sample code

---
+++ dev-books/오브젝트/sample/ticket/ticket/src/main/kotlin/com/sample/01-Sample.kt
@@ -0,0 +1,74 @@
+//package com.sample
+//
+//import java.time.LocalDateTime
+//
+//class Invitation(
+//  val `when`: LocalDateTime
+//)
+//
+//class Ticket(
+//  val fee: Long
+//)
+//
+//class Bag(
+//  var amount: Long,
+//  val invitation: Invitation? = null,
+//  var ticket: Ticket? = null
+//) {
+//  fun hasInvitation() = invitation != null
+//
+//  fun hasTicket() = ticket != null
+//
+//  fun minusAmount(amount: Long) {
+//    this.amount -= amount
+//  }
+//
+//  fun plusAmount(amount: Long) {
+//    this.amount += amount
+//  }
+//}
+//
+//class Audience(
+//  val bag: Bag
+//)
+//
+//class TicketOffice(
+//  var amount: Long,
+//  val tickets: MutableList<Ticket> = mutableListOf()
+//) {
+//  fun getTicket(): Ticket {
+//    return tickets.removeAt(0)
+//  }
+//
+//  fun minusAmount(amount: Long) {
+//    this.amount -= amount
+//  }
+//
+//  fun plusAmount(amount: Long) {
+//    this.amount += amount
+//  }
+//}
+//
+//class TicketSeller(
+//  val ticketOffice: TicketOffice
+//)
+//
+///**
+// * audience 와  audience 의 bag, ticketSeller 와 ticketSeller 의 ticketOffice 까지 theater 가 직접 접근하며 통제하고 변경하고 있다.
+// * audience 가 변경되는 ticketSeller 가 변경되는 이와 결합된 Theater 까지 변경될 수 있다.
+// */
+//class Theater(
+//  val ticketSeller: TicketSeller
+//) {
+//  fun enter(audience: Audience) {
+//    if (audience.bag.hasInvitation()) {
+//      val ticket = ticketSeller.ticketOffice.getTicket()
+//      audience.bag.ticket = ticket
+//    } else {
+//      val ticket = ticketSeller.ticketOffice.getTicket()
+//      audience.bag.minusAmount(ticket.fee)
+//      ticketSeller.ticketOffice.plusAmount(ticket.fee)
+//      audience.bag.ticket = ticket
+//    }
+//  }
+//}
---
+++ dev-books/오브젝트/sample/ticket/ticket/src/main/kotlin/com/sample/02-advanced.kt
@@ -0,0 +1,81 @@
+package com.sample
+
+import java.time.LocalDateTime
+
+class Invitation(
+  val `when`: LocalDateTime
+)
+
+class Ticket(
+  val fee: Long
+)
+
+class Bag(
+  var amount: Long,
+  val invitation: Invitation? = null,
+  var ticket: Ticket? = null
+) {
+  fun hasInvitation() = invitation != null
+
+  fun hasTicket() = ticket != null
+
+  fun minusAmount(amount: Long) {
+    this.amount -= amount
+  }
+
+  fun plusAmount(amount: Long) {
+    this.amount += amount
+  }
+}
+
+/**
+ * audience 자신의 bag 에 접근해서 티켓이 있는지 확인하고 금액을 지불하는 것은 자기 자신의 책임이다.
+ */
+class Audience(
+  private val bag: Bag // audience 의 bag 은 private 으로 외부에서 접근을 막았다. (캡슐화)
+) {
+  fun buy(ticket: Ticket): Long {
+    return if (bag.hasInvitation()) {
+      bag.ticket = ticket
+      0L
+    } else {
+      bag.ticket = ticket
+      bag.minusAmount(ticket.fee)
+      ticket.fee
+    }
+  }
+}
+
+class TicketOffice(
+  var amount: Long,
+  val tickets: MutableList<Ticket> = mutableListOf()
+) {
+  fun getTicket(): Ticket {
+    return tickets.removeAt(0)
+  }
+
+  fun minusAmount(amount: Long) {
+    this.amount -= amount
+  }
+
+  fun plusAmount(amount: Long) {
+    this.amount += amount
+  }
+}
+
+class TicketSeller(
+  private val ticketOffice: TicketOffice // TicketSeller 의 ticketOffice 은 private 으로 외부에서 접근을 막았다. (캡슐화)
+) {
+
+  fun sellTo(audience: Audience) {
+    ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()))
+  }
+}
+
+class Theater(
+  val ticketSeller: TicketSeller
+) {
+  fun enter(audience: Audience) {
+    ticketSeller.sellTo(audience)
+  }
+}
