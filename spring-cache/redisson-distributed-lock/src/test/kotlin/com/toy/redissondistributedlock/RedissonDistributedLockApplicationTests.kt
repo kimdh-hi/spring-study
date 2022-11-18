e85acde94d2b208cf6d665ee4f3e3dc23e6b3868
daehyunKim <zbeld123@gmail.com>
2022-11-18 14:25:59 +0900

    chore(redisson): init

---
+++ spring-cache/redisson-distributed-lock/src/test/kotlin/com/toy/redissondistributedlock/RedissonDistributedLockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.redissondistributedlock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedissonDistributedLockApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
