4b8377a8d719b73d7dc0231031fcab59a4882d96
daehyunKim <zbeld123@gmail.com>
2022-11-21 21:07:32 +0900

    test(toy): test using Spock

--- lecture/fs-pharmacy/pharmacy/build.gradle.kts
+++ lecture/fs-pharmacy/pharmacy/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   id("org.springframework.boot") version "2.7.5"
   id("io.spring.dependency-management") version "1.0.15.RELEASE"
+  id("groovy")
   kotlin("jvm") version "1.6.21"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
@@ -23,7 +24,14 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
+  //spock
+  testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
+  testImplementation("org.spockframework:spock-spring:2.3-groovy-4.0")
+
+  // 런타임에 클래스 기반 spock mock 을 만들기 위
+  testImplementation("net.bytebuddy:byte-buddy:1.12.10")
 }
 
 tasks.withType<KotlinCompile> {
--- lecture/fs-pharmacy/pharmacy/src/main/kotlin/com/lecture/pharmacy/api/dto/KakaoResponseDto.kt
+++ lecture/fs-pharmacy/pharmacy/src/main/kotlin/com/lecture/pharmacy/api/dto/KakaoResponseDto.kt
@@ -1,7 +1,11 @@
 package com.lecture.pharmacy.api.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class KakaoResponseDto(
+  @JsonProperty("meta")
   val metaDto: MetaDto,
 
+  @JsonProperty("documents")
   val documents: List<DocumentDto>
 )
