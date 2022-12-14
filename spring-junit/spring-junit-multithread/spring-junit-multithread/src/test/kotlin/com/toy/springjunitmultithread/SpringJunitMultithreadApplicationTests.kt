c0b3ed9fa30e5bfecfe8aab14af28a271a7ffff2
daehyunKim <zbeld123@gmail.com>
2022-12-14 11:44:11 +0900

    chore(junit-multithread): init

---
+++ spring-junit/spring-junit-multithread/spring-junit-multithread/src/test/kotlin/com/toy/springjunitmultithread/SpringJunitMultithreadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springjunitmultithread
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJunitMultithreadApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
