9bc08ad8d5e97e323a8b74c3ce8ae0fdff31c6a0
daehyunKim <zbeld123@gmail.com>
2022-12-21 13:05:11 +0900

    chore(websocket): spring websocket stomp init

---
+++ spring-websocket/spring-stomp/spring-stomp/src/test/kotlin/com/toy/springstomp/SpringStompApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springstomp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringStompApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
