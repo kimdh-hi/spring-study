85648ebe3061c3d57e1b124da8b991eee5275ecb
daehyunKim <zbeld123@gmail.com>
2022-12-21 10:14:16 +0900

    chore(websocket): init

---
+++ spring-websocket/spring-websocket/src/main/kotlin/com/toy/springwebsocket/SpringWebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springwebsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebsocketApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringWebsocketApplication>(*args)
+}
