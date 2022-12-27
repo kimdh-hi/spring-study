5ff37701a8306518f5b956018a9018f289e3a720
daehyunKim <zbeld123@gmail.com>
2022-12-27 21:35:39 +0900

    chore(graphql): webflux graphql init

---
+++ spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/SpringWebfluxGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springwebfluxgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxGraphqlApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringWebfluxGraphqlApplication>(*args)
+}
