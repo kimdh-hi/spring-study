9add2106b1a3bfa35607f9088fe734f38382cdf4
daehyunKim <zbeld123@gmail.com>
2022-12-28 19:47:28 +0900

    chore(graphql): mv package

--- spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/sec01/lec01/GraphqlController.kt
+++ spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/sec01/lec01/GraphqlController.kt
@@ -1,4 +1,4 @@
-package com.toy.springwebfluxgraphql
+package com.toy.springwebfluxgraphql.sec01.lec01
 
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
