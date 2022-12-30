e1bea3269cecca27ada89ca2ca8b1535c66f1045
daehyunKim <zbeld123@gmail.com>
2022-12-30 16:45:27 +0900

    chore(oauth2): init

---
+++ lecture/inflearn-spring-security-oauth2/inflearn-spring-security-oauth2/src/main/kotlin/com/lecture/inflearnspringsecurityoauth2/InflearnSpringSecurityOauth2Application.kt
@@ -0,0 +1,11 @@
+package com.lecture.inflearnspringsecurityoauth2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnSpringSecurityOauth2Application
+
+fun main(args: Array<String>) {
+  runApplication<InflearnSpringSecurityOauth2Application>(*args)
+}
---
+++ lecture/inflearn-spring-security-oauth2/inflearn-spring-security-oauth2/src/test/kotlin/com/lecture/inflearnspringsecurityoauth2/InflearnSpringSecurityOauth2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.inflearnspringsecurityoauth2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InflearnSpringSecurityOauth2ApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
