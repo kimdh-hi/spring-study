b6ca5fa335337f2ebfb3ac64e9d3fd0da6ded084
daehyunKim <zbeld123@gmail.com>
2023-01-04 17:12:09 +0900

    chore(jpa): init

---
+++ spring-jpa/jpa-field-encrypt/jpa-field-encrypt/src/main/kotlin/com/toy/jpafieldencrypt/JpaFieldEncryptApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.jpafieldencrypt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaFieldEncryptApplication
+
+fun main(args: Array<String>) {
+	runApplication<JpaFieldEncryptApplication>(*args)
+}
---
+++ spring-jpa/jpa-field-encrypt/jpa-field-encrypt/src/test/kotlin/com/toy/jpafieldencrypt/JpaFieldEncryptApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.jpafieldencrypt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaFieldEncryptApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
