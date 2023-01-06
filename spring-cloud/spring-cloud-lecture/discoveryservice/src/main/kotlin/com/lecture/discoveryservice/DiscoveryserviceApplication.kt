15e557e0094722d51f2a1d9da07e6b33d76c790d
daehyunKim <zbeld123@gmail.com>
2023-01-07 00:17:40 +0900

    feat(cloud): add user-service, register to eureka dicovery server

--- spring-cloud/spring-cloud-lecture/discoveryservice/src/main/kotlin/com/lecture/discoveryservice/DiscoveryserviceApplication.kt
+++ spring-cloud/spring-cloud-lecture/discoveryservice/src/main/kotlin/com/lecture/discoveryservice/DiscoveryserviceApplication.kt
@@ -2,8 +2,10 @@ package com.lecture.discoveryservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryserviceApplication
 
 fun main(args: Array<String>) {
---
+++ spring-cloud/spring-cloud-lecture/user-service/src/main/kotlin/com/lecture/userservice/UserServiceApplication.kt
@@ -0,0 +1,13 @@
+package com.lecture.userservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+class UserServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<UserServiceApplication>(*args)
+}
