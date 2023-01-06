67c6b5309dc72595a3d92351a8dc97e86d3c4dbf
daehyunKim <zbeld123@gmail.com>
2023-01-06 23:21:30 +0900

    feat(cloud): eureka discovery server service init

---
+++ spring-cloud/spring-cloud-lecture/discoveryservice/discoveryservice/src/main/kotlin/com/lecture/discoveryservice/DiscoveryserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.discoveryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscoveryserviceApplication
+
+fun main(args: Array<String>) {
+  runApplication<DiscoveryserviceApplication>(*args)
+}
---
+++ spring-cloud/spring-cloud-lecture/discoveryservice/discoveryservice/src/test/kotlin/com/lecture/discoveryservice/DiscoveryserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.discoveryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
