543019f99d1550522002c978c942f475dfe50ae8
daehyunKim <zbeld123@gmail.com>
2023-01-10 19:52:46 +0900

    chore(jpa): init

---
+++ spring-jpa/jpa-event-listener/jpa-event-listener/src/test/kotlin/com/toy/jpaeventlistener/JpaEventListenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.jpaeventlistener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaEventListenerApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
