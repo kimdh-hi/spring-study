d41b335aea00ce4e067c0464a7946ef0ff8a509d
daehyunKim <zbeld123@gmail.com>
2023-01-12 18:21:30 +0900

    chore(oauth2): resource server init

---
+++ lecture/inflearn-spring-security-oauth2/oauth2-resource-server/oauth2-resource-server/src/test/kotlin/com/lecture/oauth2resourceserver/Oauth2ResourceServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.oauth2resourceserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2ResourceServerApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
