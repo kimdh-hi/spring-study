bd7e576e8a324df0f5406aa4566cdd5d37b3c11a
daehyunKim <zbeld123@gmail.com>
2023-01-16 11:26:33 +0900

    chore(cloud): jaxb-api dependency

--- spring-cloud/spring-cloud-lecture/spring-cloud-msa/user-service/build.gradle.kts
+++ spring-cloud/spring-cloud-lecture/spring-cloud-msa/user-service/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("io.jsonwebtoken:jjwt:0.9.1")
+  implementation("javax.xml.bind:jaxb-api")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
