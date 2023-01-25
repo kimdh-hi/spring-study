3afdbd9448ca7996801fc288512628b64bdc7954
daehyunKim <zbeld123@gmail.com>
2023-01-25 18:26:12 +0900

    feat(kotest): funcSpec

---
+++ kotest/src/test/kotlin/com/example/kotest/04-FunSpec.kt
@@ -0,0 +1,18 @@
+package com.example.kotest
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class `04-FunSpec`: FunSpec({
+
+  test("test 입니다") {
+    //given
+    val data = 1
+
+    //when
+    val result = data == 1
+
+    //then
+    result.shouldBe(true)
+  }
+})
