d5f2fd94087d0c6676998f7cf212eb406d7c717d
daehyunKim <zbeld123@gmail.com>
2023-01-25 21:17:21 +0900

    chore(cloud-stream): init

---
+++ spring-cloud-stream/src/main/kotlin/com/toy/springcloudstream/SpringCloudStreamApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springcloudstream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudStreamApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringCloudStreamApplication>(*args)
+}
