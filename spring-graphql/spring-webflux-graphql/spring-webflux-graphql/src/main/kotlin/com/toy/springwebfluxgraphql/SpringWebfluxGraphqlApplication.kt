faa4a896f11adce7ed378ca1c443e07fa5d9ab09
daehyunKim <zbeld123@gmail.com>
2023-01-28 18:23:29 +0900

    feat(graphql): get requestHeader using WebGraphQlInterceptor

--- spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/SpringWebfluxGraphqlApplication.kt
+++ spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/SpringWebfluxGraphqlApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
-@SpringBootApplication(scanBasePackages = ["com.toy.springwebfluxgraphql.lec14"])
-@EnableR2dbcRepositories(basePackages = ["com.toy.springwebfluxgraphql.lec14"])
+@SpringBootApplication(scanBasePackages = ["com.toy.springwebfluxgraphql.lec04"])
+@EnableR2dbcRepositories(basePackages = ["com.toy.springwebfluxgraphql.lec04"])
 class SpringWebfluxGraphqlApplication
 
 fun main(args: Array<String>) {
--- spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/lec14/controller/CustomerController.kt
+++ spring-graphql/spring-webflux-graphql/spring-webflux-graphql/src/main/kotlin/com/toy/springwebfluxgraphql/lec14/controller/CustomerController.kt
@@ -4,6 +4,8 @@ import com.toy.springwebfluxgraphql.lec14.exceptions.Errors
 import com.toy.springwebfluxgraphql.lec14.service.CustomerService
 import com.toy.springwebfluxgraphql.lec14.vo.CustomerDeleteResponseVO
 import com.toy.springwebfluxgraphql.lec14.vo.CustomerVO
+import graphql.schema.DataFetchingEnvironment
+import org.slf4j.LoggerFactory
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -17,8 +19,14 @@ class CustomerController(
   private val customerService: CustomerService
 ) {
 
+  private val log = LoggerFactory.getLogger(javaClass)
+
   @QueryMapping
-  fun customers(): Flux<CustomerVO> = customerService.findAll()
+  fun customers(environment: DataFetchingEnvironment): Flux<CustomerVO> {
+    val someHeader = environment.graphQlContext.get<String>("some-header")
+    log.info("someHeader: {}", someHeader)
+    return customerService.findAll()
+  }
 
   @QueryMapping
   fun customerById(@Argument id: Int): Mono<CustomerVO> = customerService.findById(id)
