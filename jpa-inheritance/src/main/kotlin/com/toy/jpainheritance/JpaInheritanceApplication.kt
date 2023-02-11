fcc1dcd2f65118d9977d71f8a603b81c90c0871d
daehyunKim <zbeld123@gmail.com>
2023-02-11 16:36:34 +0900

    chore(jpa): jpa inheritance, init

---
+++ jpa-inheritance/src/main/kotlin/com/toy/jpainheritance/JpaInheritanceApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.jpainheritance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaInheritanceApplication
+
+fun main(args: Array<String>) {
+  runApplication<JpaInheritanceApplication>(*args)
+}
---
+++ jpa-inheritance/src/test/kotlin/com/toy/jpainheritance/JpaInheritanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.jpainheritance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaInheritanceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
