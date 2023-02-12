3a58146eb4e90c3438b8a71917718159f8e3acb6
daehyunKim <zbeld123@gmail.com>
2023-02-12 18:19:11 +0900

    chore(integration-mqtt): init

---
+++ spring-mqtt/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.springframework.boot") version "3.0.2"
+  id("io.spring.dependency-management") version "1.1.0"
+  kotlin("jvm") version "1.7.22"
+  kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.toy"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-integration")
+  implementation("org.springframework.integration:spring-integration-mqtt")
+  implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.springframework.integration:spring-integration-http")
+
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.integration:spring-integration-test")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "17"
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
