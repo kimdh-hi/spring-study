0bea2b18ed750dd9dca0412c1e5dded885f03c61
daehyunKim <zbeld123@gmail.com>
2023-02-15 14:45:16 +0900

    chore(exposed): init

---
+++ spring-exposed/src/test/kotlin/com/toy/springexposed/SpringExposedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springexposed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringExposedApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
