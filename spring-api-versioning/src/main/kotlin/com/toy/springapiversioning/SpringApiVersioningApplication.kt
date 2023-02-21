fdd20677bcee2dfaba29157de23afbae58ddf9e8
daehyunKim <zbeld123@gmail.com>
2023-02-22 08:40:14 +0900

    chore(api-versioning): init

---
+++ spring-api-versioning/src/main/kotlin/com/toy/springapiversioning/SpringApiVersioningApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springapiversioning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringApiVersioningApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringApiVersioningApplication>(*args)
+}
