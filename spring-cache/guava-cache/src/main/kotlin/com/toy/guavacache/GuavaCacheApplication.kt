47f03d1179f3dfe9d62ce7b45304bf9630ca3c73
daehyunKim <zbeld123@gmail.com>
2023-02-24 15:28:05 +0900

    chore(guava-cache): init

---
+++ spring-cache/guava-cache/src/main/kotlin/com/toy/guavacache/GuavaCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.guavacache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuavaCacheApplication
+
+fun main(args: Array<String>) {
+  runApplication<GuavaCacheApplication>(*args)
+}
