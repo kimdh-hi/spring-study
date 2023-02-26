7c601458263785882676830108a01b4451f84637
daehyunKim <zbeld123@gmail.com>
2023-02-26 20:12:57 +0900

    chore(bucket4j): init

---
+++ spring-bucket4j/src/test/kotlin/com/toy/springbucket4j/SpringBucket4jApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springbucket4j
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBucket4jApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
