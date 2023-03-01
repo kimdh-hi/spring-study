415c49a5c49714538981a576241c9dc4f921351f
daehyunKim <zbeld123@gmail.com>
2023-03-01 18:01:56 +0900

    chore(neo4j): init

---
+++ spring-neo4j/src/main/kotlin/com/toy/springneo4j/SpringNeo4jApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springneo4j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringNeo4jApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringNeo4jApplication>(*args)
+}
---
+++ spring-neo4j/src/test/kotlin/com/toy/springneo4j/SpringNeo4jApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springneo4j
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringNeo4jApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
