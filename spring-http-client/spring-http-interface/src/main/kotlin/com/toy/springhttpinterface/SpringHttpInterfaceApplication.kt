055f4497c36de1c4a01639f2546a2fa9b5971c9f
daehyunKim <zbeld123@gmail.com>
2023-03-14 08:04:31 +0900

    chore(http-interface) init

---
+++ spring-http-client/spring-http-interface/src/main/kotlin/com/toy/springhttpinterface/SpringHttpInterfaceApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springhttpinterface
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringHttpInterfaceApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringHttpInterfaceApplication>(*args)
+}
---
+++ spring-http-client/spring-http-interface/src/test/kotlin/com/toy/springhttpinterface/SpringHttpInterfaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springhttpinterface
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringHttpInterfaceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
