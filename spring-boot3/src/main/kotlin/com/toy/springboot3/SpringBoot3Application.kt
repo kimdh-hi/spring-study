52b2427589b78b04f3c964cda5902c67ae82ac3c
daehyunKim <zbeld123@gmail.com>
2023-03-16 14:58:32 +0900

    chore(boot3): init

---
+++ spring-boot3/src/main/kotlin/com/toy/springboot3/SpringBoot3Application.kt
@@ -0,0 +1,11 @@
+package com.toy.springboot3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoot3Application
+
+fun main(args: Array<String>) {
+  runApplication<SpringBoot3Application>(*args)
+}
---
+++ spring-boot3/src/test/kotlin/com/toy/springboot3/SpringBoot3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springboot3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBoot3ApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
