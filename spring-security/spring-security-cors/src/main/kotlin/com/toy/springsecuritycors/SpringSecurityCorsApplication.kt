87bd4c81bf7f6e3b148c3b5277ce9ce60b730118
daehyunKim <zbeld123@gmail.com>
2023-03-23 10:28:27 +0900

    chore(security): spring-security cors sample init

---
+++ spring-security/spring-security-cors/src/main/kotlin/com/toy/springsecuritycors/SpringSecurityCorsApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springsecuritycors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityCorsApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringSecurityCorsApplication>(*args)
+}
