376188b20dfde159aa1f460b67864421f99317b5
daehyunKim <zbeld123@gmail.com>
2023-04-02 22:42:36 +0900

    feat(inflearn-boot): read property

--- lecture/inflearn-spring-boot/external-read/src/main/java/hello/ExternalReadApplication.java
+++ lecture/inflearn-spring-boot/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,12 @@
 package hello;
 
+import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"hello.datasource"})
+@Import(MyDataSourceValueConfig.class)
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
