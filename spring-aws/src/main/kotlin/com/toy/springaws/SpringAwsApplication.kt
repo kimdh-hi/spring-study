fd3988c1efa354e2ed99971bb5ed878be277c368
daehyunKim <zbeld123@gmail.com>
2023-04-12 18:21:08 +0900

    chore(spring-aws): init

---
+++ spring-aws/src/main/kotlin/com/toy/springaws/SpringAwsApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springaws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAwsApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringAwsApplication>(*args)
+}
---
+++ spring-aws/src/test/kotlin/com/toy/springaws/SpringAwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAwsApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
