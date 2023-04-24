7d9bb52445eea7c05ac9d1934693e4f6ee4f8bb1
daehyunKim <zbeld123@gmail.com>
2023-04-24 17:54:45 +0900

    feat(kafka): core

--- lecture/udemy-kafka/reactive-kafka/build.gradle.kts
+++ lecture/udemy-kafka/reactive-kafka/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("io.projectreactor.kafka:reactor-kafka:1.3.16")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
--- lecture/udemy-kafka/reactive-kafka/src/main/kotlin/com/lecture/reactivekafka/sec01/Lec01KafkaConsumer.kt
+++ lecture/udemy-kafka/reactive-kafka/src/main/kotlin/com/lecture/reactivekafka/sec01/Lec01KafkaConsumer.kt
@@ -1,4 +1,29 @@
 package com.lecture.reactivekafka.sec01
 
-class Lec01KafkaConsumer {
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.LoggerFactory
+import reactor.kafka.receiver.KafkaReceiver
+import reactor.kafka.receiver.ReceiverOptions
+
+private val log = LoggerFactory.getLogger(Lec01KafkaConsumer::class.java)
+
+class Lec01KafkaConsumer
+
+fun main() {
+
+  val consumerConfig = mapOf(
+    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class,
+    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class,
+    ConsumerConfig.GROUP_ID_CONFIG to "demo-group"
+  )
+
+  val receiverOptions = ReceiverOptions.create<String, Any>(consumerConfig)
+    .subscription(listOf("order-event"))
+
+  KafkaReceiver.create(receiverOptions)
+    .receive()
+    .doOnNext { log.info("key={}, value={}", it.key(), it.value()) }
+    .subscribe()
 }
