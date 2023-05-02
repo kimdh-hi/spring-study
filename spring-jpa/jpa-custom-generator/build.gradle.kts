96693d2abb6af4c2a9cca66e1f9066c2fe8145d6
김대현 <dhkim2@rsupport.com>
2023-05-02 19:06:43 +0900

    chore(jpa): boot3 version up

--- spring-jpa/jpa-custom-generator/build.gradle.kts
+++ spring-jpa/jpa-custom-generator/build.gradle.kts
@@ -1,22 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "2.7.8"
-  id("io.spring.dependency-management") version "1.0.15.RELEASE"
-  kotlin("jvm") version "1.6.21"
-  kotlin("plugin.spring") version "1.6.21"
-  kotlin("plugin.jpa") version "1.6.21"
-  kotlin("plugin.noarg") version "1.6.21"
+  id("org.springframework.boot") version "3.0.6"
+  id("io.spring.dependency-management") version "1.1.0"
+  kotlin("jvm") version "1.7.22"
+  kotlin("plugin.spring") version "1.7.22"
+  kotlin("plugin.jpa") version "1.7.22"
+  kotlin("plugin.noarg") version "1.7.22"
 }
 
 allOpen {
-  annotation("javax.persistence.Entity")
-  annotation("javax.persistence.Embeddable")
-  annotation("javax.persistence.MappedSuperclass")
+  annotation("jakarta.persistence.Entity")
+  annotation("jakarta.persistence.Embeddable")
+  annotation("jakarta.persistence.MappedSuperclass")
 }
 
 noArg {
-  annotation("javax.persistence.Entity")
+  annotation("jakarta.persistence.Entity")
 }
 
 group = "com.toy"
@@ -30,7 +30,9 @@ repositories {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("com.fasterxml.uuid:java-uuid-generator:4.1.0")
+
+  //https://github.com/cowtowncoder/java-uuid-generator
+  implementation("com.fasterxml.uuid:java-uuid-generator:4.1.1")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
