71631a8a8a0afad35a205f15e887330c274fc7c0
daehyunKim <zbeld123@gmail.com>
2023-05-08 09:10:05 +0900

    chore(jpa): emoji test init

---
+++ spring-jpa/jpa-emoji/src/main/kotlin/com/toy/jpaemoji/JpaEmojiApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.jpaemoji
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaEmojiApplication
+
+fun main(args: Array<String>) {
+  runApplication<JpaEmojiApplication>(*args)
+}
---
+++ spring-jpa/jpa-emoji/src/test/kotlin/com/toy/jpaemoji/JpaEmojiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.jpaemoji
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaEmojiApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
