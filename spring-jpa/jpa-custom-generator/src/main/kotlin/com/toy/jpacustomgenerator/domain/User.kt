d26025f55999a1d55ce6b5da8c7db3908647331e
daehyunKim <zbeld123@gmail.com>
2023-05-12 15:50:54 +0900

    feat(jpa): timebased uuid paging test

--- spring-jpa/jpa-custom-generator/src/main/kotlin/com/toy/jpacustomgenerator/domain/User.kt
+++ spring-jpa/jpa-custom-generator/src/main/kotlin/com/toy/jpacustomgenerator/domain/User.kt
@@ -1,23 +1,18 @@
 package com.toy.jpacustomgenerator.domain
 
-import com.toy.jpacustomgenerator.common.CustomIdGenerator
-import com.toy.jpacustomgenerator.common.TimebasedUUID
-import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.GenericGenerator
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity(name = "tb_user")
 @Table
 class User(
   @Id
-  @GenericGenerator(name = "id", strategy = "com.toy.jpacustomgenerator.generator.CustomIdGenerator")
+  @GenericGenerator(name = "idGenerator", strategy = "com.toy.jpacustomgenerator.common.CustomIdGenerator")
+  @GeneratedValue(generator = "idGenerator")
   var id: String = "",
 
   var name: String,
