3cf33afc62907122d74d2b7191463ad70c168acd
daehyunKim <zbeld123@gmail.com>
2023-06-25 14:55:21 +0900

    feat(mvc): multipartfile list mapping

--- spring-mvc/src/main/kotlin/com/toy/springmvc/controller/MultipartFileListTestController.kt
+++ spring-mvc/src/main/kotlin/com/toy/springmvc/controller/MultipartFileListTestController.kt
@@ -1,23 +1,15 @@
 package com.toy.springmvc.controller
 
 import com.toy.springmvc.config.NoArg
-import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
-import org.springframework.validation.Errors
-import org.springframework.validation.Validator
-import org.springframework.web.bind.WebDataBinder
-import org.springframework.web.bind.annotation.InitBinder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping("/multipartFile-list")
+@RequestMapping("/multipartFileList")
 class MultipartFileListTestController {
-
-    private val log = LoggerFactory.getLogger(javaClass)
-
 
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun test(request: EmoticonSaveRequest): String {
@@ -26,6 +18,7 @@ class MultipartFileListTestController {
     }
 }
 
+@NoArg
 data class EmoticonSaveRequest(
     val i18ns: MutableList<EmoticonI18nRequest> = mutableListOf()
 )
@@ -34,5 +27,5 @@ data class EmoticonSaveRequest(
 data class EmoticonI18nRequest(
     var name: String,
     var representativeFile: MultipartFile,
-//    val emoticonFiles: List<MultipartFile>
+    var emoticonFiles: MutableList<MultipartFile> = mutableListOf()
 )
