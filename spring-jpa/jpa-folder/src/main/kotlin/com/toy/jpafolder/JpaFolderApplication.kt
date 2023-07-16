693e509623ead9d53748674031cbfb3b854d73c3
daehyunKim <zbeld123@gmail.com>
2023-07-16 14:14:37 +0900

    chore(jpa-folder): init

---
+++ spring-jpa/jpa-folder/src/main/kotlin/com/toy/jpafolder/JpaFolderApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.jpafolder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaFolderApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaFolderApplication>(*args)
+}
---
+++ spring-jpa/jpa-folder/src/test/kotlin/com/toy/jpafolder/JpaFolderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.jpafolder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaFolderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
