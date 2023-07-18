d026b48506deb876c176e9d13eedca68279d1d49
daehyunKim <zbeld123@gmail.com>
2023-07-18 15:38:12 +0900

    chore(mobile): spring-moblie sample init

---
+++ spring-mobile/src/main/kotlin/com/toy/springmobile/SpringMobileApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springmobile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMobileApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringMobileApplication>(*args)
+}
---
+++ spring-mobile/src/test/kotlin/com/toy/springmobile/SpringMobileApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springmobile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMobileApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
