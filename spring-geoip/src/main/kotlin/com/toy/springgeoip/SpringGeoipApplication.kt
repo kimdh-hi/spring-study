40a7d5a7c2389b17c695c8f0a11a15743180506a
daehyunKim <zbeld123@gmail.com>
2023-07-20 09:18:27 +0900

    chore(geoip): sample init

---
+++ spring-geoip/src/main/kotlin/com/toy/springgeoip/SpringGeoipApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springgeoip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGeoipApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringGeoipApplication>(*args)
+}
