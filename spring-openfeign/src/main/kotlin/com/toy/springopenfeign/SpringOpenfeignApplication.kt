c6e3e1911fe44a04a6fcd7f23a268f357b9c1053
daehyunKim <zbeld123@gmail.com>
2023-07-21 14:05:00 +0900

    chore(openfeign): sample init

---
+++ spring-openfeign/src/main/kotlin/com/toy/springopenfeign/SpringOpenfeignApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springopenfeign
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringOpenfeignApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringOpenfeignApplication>(*args)
+}
---
+++ spring-openfeign/src/test/kotlin/com/toy/springopenfeign/SpringOpenfeignApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springopenfeign
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringOpenfeignApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
