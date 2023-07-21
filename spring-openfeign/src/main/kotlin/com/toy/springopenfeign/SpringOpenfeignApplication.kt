adb2ccbd15f53aa0e20b83b5fcf64a020dccbb13
daehyunKim <zbeld123@gmail.com>
2023-07-21 14:30:25 +0900

    refactor(openfeign): remove @EnableFeignClients from main class

--- spring-openfeign/src/main/kotlin/com/toy/springopenfeign/SpringOpenfeignApplication.kt
+++ spring-openfeign/src/main/kotlin/com/toy/springopenfeign/SpringOpenfeignApplication.kt
@@ -2,10 +2,8 @@ package com.toy.springopenfeign
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableFeignClients
 class SpringOpenfeignApplication
 
 fun main(args: Array<String>) {
--- spring-openfeign/src/main/kotlin/com/toy/springopenfeign/config/OpenFeignConfig.kt
+++ spring-openfeign/src/main/kotlin/com/toy/springopenfeign/config/OpenFeignConfig.kt
@@ -4,5 +4,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableFeignClients(basePackages = ["com.toy.springopenfeign"]) // main 클래스가 아닌 곳에서 설정시 basePackages 설정필요
 class OpenFeignConfig {
 }
