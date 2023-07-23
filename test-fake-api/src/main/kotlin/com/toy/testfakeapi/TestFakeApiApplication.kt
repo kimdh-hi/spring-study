7ce162aa0d56a0c1eb5be7305eabd179d15d9b77
daehyunKim <zbeld123@gmail.com>
2023-07-23 10:05:56 +0900

    chore: init fake api

---
+++ test-fake-api/src/main/kotlin/com/toy/testfakeapi/TestFakeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.testfakeapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestFakeApiApplication
+
+fun main(args: Array<String>) {
+  runApplication<TestFakeApiApplication>(*args)
+}
---
+++ test-fake-api/src/test/kotlin/com/toy/testfakeapi/TestFakeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.testfakeapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestFakeApiApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
