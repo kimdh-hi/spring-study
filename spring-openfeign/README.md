91043190c07f543b0f9cfccb0e64a4b6a3ad8f33
daehyunKim <zbeld123@gmail.com>
2023-07-23 10:02:54 +0900

    feat(openfeign): retry

---
+++ spring-openfeign/README.md
@@ -0,0 +1,6 @@
+## Spring OpenFeign
+
+재시도
+- default: Retryer.NEVER_RETRY (재시도 안 함)
+- Feign 에 제공하는 Retryer는 IO exception 이 발생한 경우에만 동작
+  - IO exception 외 다른 예외에 대해서 재시도를 원한다면 spring-retry 사용 등 다른 설정 필요
--- spring-openfeign/src/main/kotlin/com/toy/springopenfeign/config/OpenFeignConfig.kt
+++ spring-openfeign/src/main/kotlin/com/toy/springopenfeign/config/OpenFeignConfig.kt
@@ -1,9 +1,21 @@
 package com.toy.springopenfeign.config
 
+import feign.Retryer
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
 
 @Configuration
 @EnableFeignClients(basePackages = ["com.toy.springopenfeign"]) // main 클래스가 아닌 곳에서 설정시 basePackages 설정필요
 class OpenFeignConfig {
+
+  @Bean
+  fun retryer(): Retryer.Default {
+    // 최초 0.1초 대기후 재요청
+    // 매 재요청마다 대기시간 1초 증가
+    // 최대 5번 재시도
+    return Retryer.Default(100L, TimeUnit.SECONDS.toMillis(1L), 5)
+  }
 }
