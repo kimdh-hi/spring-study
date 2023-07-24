4f0356a8b2c0b2b222522ff5e73f82064baa6833
daehyunKim <zbeld123@gmail.com>
2023-07-24 12:50:26 +0900

    chore(wiremock): sample init

---
+++ spring-wiremock/src/main/kotlin/com/toy/springwiremock/SpringWiremockApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springwiremock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWiremockApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringWiremockApplication>(*args)
+}
