6196b8875fee9bb4bd580dde53e006459ad4d114
daehyunKim <zbeld123@gmail.com>
2023-08-21 09:24:48 +0900

    chore(redis) version up, clean sample project

--- spring-cache/spring-redis/build.gradle.kts
+++ spring-cache/spring-redis/build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "2.7.0"
-  id("io.spring.dependency-management") version "1.0.11.RELEASE"
-  kotlin("jvm") version "1.7.20"
-  kotlin("plugin.spring") version "1.7.20"
-  kotlin("plugin.jpa") version "1.7.20"
-  kotlin("plugin.noarg") version "1.7.20"
-  kotlin("plugin.serialization") version "1.7.20"
+  id("org.springframework.boot") version "3.1.2"
+  id("io.spring.dependency-management") version "1.1.2"
+  kotlin("jvm") version "1.8.22"
+  kotlin("plugin.spring") version "1.8.22"
+  kotlin("plugin.jpa") version "1.8.22"
+  kotlin("plugin.noarg") version "1.8.22"
+  kotlin("plugin.serialization") version "1.8.22"
 }
 
 group = "com.toy"
@@ -34,7 +34,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
   implementation("org.springframework.session:spring-session-data-redis")
-  implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.core:jackson-databind")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
@@ -46,7 +45,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   runtimeOnly("com.h2database:h2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude(group = "org.slf4j", module = "slf4j-simple")}
+  testImplementation("it.ozimov::0.7.3") { exclude(group = "org.slf4j", module = "slf4j-simple")}
   testImplementation("org.testcontainers:testcontainers:1.17.6")
 }
 
