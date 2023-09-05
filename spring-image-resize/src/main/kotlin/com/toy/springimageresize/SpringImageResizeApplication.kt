c80abc42fc49381bb3dd34beff78b941386a02c3
daehyunKim <zbeld123@gmail.com>
2023-09-05 15:50:25 +0900

    chore(image-resize): init

---
+++ spring-image-resize/src/main/kotlin/com/toy/springimageresize/SpringImageResizeApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springimageresize
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringImageResizeApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringImageResizeApplication>(*args)
+}
---
+++ spring-image-resize/src/test/kotlin/com/toy/springimageresize/SpringImageResizeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springimageresize
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringImageResizeApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
