aafd4c977fb8e4b5adab6e0ac88cbd27376e6c51
daehyunKim <zbeld123@gmail.com>
2023-09-15 21:12:58 +0900

    chore(cookie): sample project init

---
+++ spring-cookie/src/main/kotlin/com/toy/springcookie/SpringCookieApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springcookie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCookieApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringCookieApplication>(*args)
+}
