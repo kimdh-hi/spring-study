6d1e7e81a7ed8b3a4f3eed5bc74f41f34a5ad0f9
daehyunKim <zbeld123@gmail.com>
2023-09-22 15:37:38 +0900

    feat(image-resize): resize using BufferedImage

--- spring-image-resize/src/main/kotlin/com/toy/springimageresize/utils/Graphics2DUtils.kt
+++ spring-image-resize/src/main/kotlin/com/toy/springimageresize/utils/Graphics2DUtils.kt
@@ -1,8 +1,16 @@
 package com.toy.springimageresize.utils
 
+import java.awt.image.BufferedImage
+
 object Graphics2DUtils {
 
-  fun resize(width: Int, height: Int) {
+  fun resize(originalImage: BufferedImage, width: Int, height: Int): BufferedImage {
+    val resizedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    resizedImage.createGraphics().apply {
+      drawImage(originalImage, 0, 0, width, height, null)
+      dispose()
+    }
 
+    return resizedImage
   }
 }
