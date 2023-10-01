bc68781574e9bb48607460596501b11af0b9c180
daehyunKim <zbeld123@gmail.com>
2023-10-01 13:13:06 +0900

    feat(actuator): git info

--- spring-actuator/build.gradle.kts
+++ spring-actuator/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   id("org.springframework.boot") version "3.1.4"
   id("io.spring.dependency-management") version "1.1.3"
+  id("com.gorylenko.gradle-git-properties") version "2.4.1" // for info - git (build 시 git.properties 생성)
   kotlin("jvm") version "1.8.22"
   kotlin("plugin.spring") version "1.8.22"
   kotlin("plugin.jpa") version "1.8.22"
