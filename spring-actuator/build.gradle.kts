2c772f50af9c1d441b1c7763c84c46c7edeea6f8
daehyunKim <zbeld123@gmail.com>
2023-10-01 13:06:08 +0900

    feat(actuator): endpoints

--- spring-actuator/build.gradle.kts
+++ spring-actuator/build.gradle.kts
@@ -6,10 +6,21 @@ plugins {
   kotlin("jvm") version "1.8.22"
   kotlin("plugin.spring") version "1.8.22"
   kotlin("plugin.jpa") version "1.8.22"
+  kotlin("plugin.noarg") version "1.8.22"
 }
 
 group = "com.toy"
 version = "0.0.1-SNAPSHOT"
+
+allOpen {
+  annotation("jakarta.persistence.Entity")
+  annotation("jakarta.persistence.Embeddable")
+  annotation("jakarta.persistence.MappedSuperclass")
+}
+
+noArg {
+  annotation("jakarta.persistence.Entity")
+}
 
 java {
   sourceCompatibility = JavaVersion.VERSION_17
@@ -24,6 +35,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  runtimeOnly("com.h2database:h2")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
