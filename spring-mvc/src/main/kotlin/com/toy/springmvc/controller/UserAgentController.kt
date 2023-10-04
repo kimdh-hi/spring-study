50350f3a966590543de565dbd616c508c19d8b91
daehyunKim <zbeld123@gmail.com>
2023-10-04 11:38:41 +0900

    feat(userAgent): userAgentUtils customising

--- spring-mvc/src/main/kotlin/com/toy/springmvc/controller/UserAgentController.kt
+++ spring-mvc/src/main/kotlin/com/toy/springmvc/controller/UserAgentController.kt
@@ -1,5 +1,6 @@
 package com.toy.springmvc.controller
 
+import eu.bitwalker.useragentutils.Manufacturer
 import eu.bitwalker.useragentutils.OperatingSystem
 import eu.bitwalker.useragentutils.UserAgent
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,8 +13,28 @@ import jakarta.servlet.http.HttpServletRequest
 class UserAgentController {
 
   @GetMapping
-  fun get(request: HttpServletRequest): OperatingSystem {
+  fun get(request: HttpServletRequest): DeviceType {
     val userAgent = UserAgent(request.getHeader("user-agent"))
-    return userAgent.operatingSystem
+    return userAgent.getDeviceType()
   }
+}
+
+fun UserAgent.getDeviceType(): DeviceType {
+  return this.operatingSystem.getDevice()
+}
+
+fun OperatingSystem.getDevice(): DeviceType {
+  return if (manufacturer == Manufacturer.MICROSOFT || manufacturer == Manufacturer.LINUX_FOUNDATION) {
+    DeviceType.WEB
+  } else if (manufacturer == Manufacturer.GOOGLE && deviceType == eu.bitwalker.useragentutils.DeviceType.MOBILE) {
+    DeviceType.AOS
+  } else if (manufacturer == Manufacturer.APPLE && deviceType == eu.bitwalker.useragentutils.DeviceType.MOBILE) {
+    DeviceType.IOS
+  } else {
+    DeviceType.UNKNOWN
+  }
+}
+
+enum class DeviceType {
+  WEB, AOS, IOS, UNKNOWN;
 }
