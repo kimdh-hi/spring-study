d388d190bf50d7726aef0b5d7d11e86bd97943d7
daehyunKim <zbeld123@gmail.com>
2023-10-17 10:40:11 +0900

    chore(mvc): typeconverter init

---
+++ lecture/spring-mvc-typeconverter/src/main/kotlin/com/lecture/springmvctypeconverter/SpringMvcTypeconverterApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.springmvctypeconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcTypeconverterApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringMvcTypeconverterApplication>(*args)
+}
---
+++ lecture/spring-mvc-typeconverter/src/test/kotlin/com/lecture/springmvctypeconverter/SpringMvcTypeconverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.springmvctypeconverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcTypeconverterApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
