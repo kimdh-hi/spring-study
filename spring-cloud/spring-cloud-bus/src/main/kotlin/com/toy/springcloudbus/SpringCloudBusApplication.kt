73aa21438251ca5d5825ba3e75d9283dbcf46ad4
daehyunKim <zbeld123@gmail.com>
2023-10-22 10:07:18 +0900

    chore(cloud): spring cloud bus sample init

---
+++ spring-cloud/spring-cloud-bus/src/main/kotlin/com/toy/springcloudbus/SpringCloudBusApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springcloudbus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudBusApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringCloudBusApplication>(*args)
+}
---
+++ spring-cloud/spring-cloud-bus/src/test/kotlin/com/toy/springcloudbus/SpringCloudBusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springcloudbus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudBusApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
