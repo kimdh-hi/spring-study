33dbd25c8205fcb13fdfce27edc815f4ccf35cf5
daehyunKim <zbeld123@gmail.com>
2023-11-02 11:08:45 +0900

    feat(aop): aspect order error... dependency circulation

---
+++ spring-aop/src/main/kotlin/com/toy/springaop/aspect/AspectV1.kt
@@ -0,0 +1,49 @@
+package com.toy.springaop.aspect
+
+import com.toy.springaop.logtrace.LogTrace
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.slf4j.LoggerFactory
+
+@Aspect
+class LogTraceAspect(
+  private val logTrace: LogTrace
+) {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  // pointcut signature
+  // private, public 모두 가능
+  @Pointcut("execution(* com.toy.springaop..*(..))")
+  private fun allServicePackage() {}
+
+  @Pointcut("execution(* * ..*Service.*(..))")
+  private fun allServiceClass() {}
+
+  @Around("allServicePackage()")
+  fun log(joinPoint: ProceedingJoinPoint): Any {
+    logTrace.start()
+
+    val result = joinPoint.proceed()
+
+    logTrace.end()
+
+    return result
+  }
+
+  @Around("allServicePackage() && allServiceClass()")
+  fun transaction(joinPoint: ProceedingJoinPoint): Any {
+    return try {
+      log.info("transaction start...")
+      val result = joinPoint.proceed()
+      log.info("transaction commit...")
+      result
+    } catch (e: Exception) {
+      log.info("transaction rollback...")
+    } finally {
+      log.info("transaction close")
+    }
+  }
+}
