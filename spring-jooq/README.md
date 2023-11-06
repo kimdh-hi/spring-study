0665638d328bf3d0752562119ce535f6d072aacd
daehyunKim <zbeld123@gmail.com>
2023-11-06 13:33:14 +0900

    chore(jooq): jooq toy project init

---
+++ spring-jooq/README.md
@@ -0,0 +1,6 @@
+## Spring jOOQ
+
+
+### reference
+- https://www.baeldung.com/spring-boot-support-for-jooq
+- https://zuminternet.github.io/JOOQ/
---
+++ spring-jooq/src/test/kotlin/com/toy/springjooq/SpringJooqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springjooq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJooqApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
