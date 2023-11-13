9b2f2282b995f9de7439be5063ee0017029ae7f6
daehyunKim <zbeld123@gmail.com>
2023-11-13 22:11:31 +0900

    chore(hibernate6): init

---
+++ spring-jpa/hibernate6/src/test/kotlin/com/toy/hibernate6/Hibernate6ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.hibernate6
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Hibernate6ApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
