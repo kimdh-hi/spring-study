1e13719d89049e7fb5eddbd68952508b81fbd400
daehyunKim <zbeld123@gmail.com>
2023-11-17 23:18:48 +0900

    chore(openfeign) sample init

---
+++ spring-openfeign/spring-openfeign-sample/serviceA/src/main/kotlin/com/toy/servicea/ServiceAApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.servicea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceAApplication
+
+fun main(args: Array<String>) {
+  runApplication<ServiceAApplication>(*args)
+}
