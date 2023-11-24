6ba5dcd7fe0b2704977269d2fe9808013612e9cc
daehyunKim <zbeld123@gmail.com>
2023-11-25 08:31:42 +0900

    feat(aop) cglib proxy implements class casting issue

--- spring-aop/src/test/kotlin/com/toy/springaop/proxy/ProxyCastingTest.kt
+++ spring-aop/src/test/kotlin/com/toy/springaop/proxy/ProxyCastingTest.kt
@@ -3,6 +3,7 @@ package com.toy.springaop.proxy
 import com.toy.springaop.service.MemberService
 import com.toy.springaop.service.MemberServiceImpl
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.springframework.aop.framework.ProxyFactory
 
@@ -21,5 +22,19 @@ class ProxyCastingTest {
 
     // jdk 동적 프록시에 의해 생성된 proxy 는 구체 타입으로 케스팅 불가능
     assertThrows<ClassCastException> { memberServiceProxy as MemberServiceImpl }
+  }
+
+  @Test
+  fun cglibProxy() {
+    val target = MemberServiceImpl()
+    val proxyFactory = ProxyFactory(target)
+
+    // cglib 프록시
+    // 구체 클래스 기반 proxy 생성
+    proxyFactory.isProxyTargetClass = true
+
+    val memberServiceProxy = proxyFactory.proxy as MemberService
+    
+    assertDoesNotThrow { memberServiceProxy as MemberServiceImpl }
   }
 }
