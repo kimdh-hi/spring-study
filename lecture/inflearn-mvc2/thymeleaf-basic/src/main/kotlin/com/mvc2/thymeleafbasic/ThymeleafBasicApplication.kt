3bffc9650bee45e77e55cfd47c85d87f97dd9d3b
daehyunKim <zbeld123@gmail.com>
2023-12-04 08:52:11 +0900

    chore: remove temp temp folder

---
+++ lecture/inflearn-mvc2/thymeleaf-basic/src/main/kotlin/com/mvc2/thymeleafbasic/ThymeleafBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.mvc2.thymeleafbasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymeleafBasicApplication
+
+fun main(args: Array<String>) {
+  runApplication<ThymeleafBasicApplication>(*args)
+}
---
+++ lecture/inflearn-mvc2/thymeleaf-basic/src/test/kotlin/com/mvc2/thymeleafbasic/ThymeleafBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mvc2.thymeleafbasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
