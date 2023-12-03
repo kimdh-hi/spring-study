7f1199f879042d120ea495603f4804ad06886625
daehyunKim <zbeld123@gmail.com>
2023-12-03 09:50:37 +0900

    feat(thymeleaf): variables

--- spring-thymeleaf/src/main/kotlin/com/toy/springthymeleaf/controller/TestController.kt
+++ spring-thymeleaf/src/main/kotlin/com/toy/springthymeleaf/controller/TestController.kt
@@ -1,18 +1,25 @@
 package com.toy.springthymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
+
 
 @Controller
 @RequestMapping("/test")
 class TestController {
 
   @GetMapping("/home")
-  fun home(vo: TestVO) = "home"
+  fun home(vo: TestVO, model: Model): String {
+    model.addAttribute("data1", vo.data1)
+    model.addAttribute("data2", vo.data2)
+    return "home"
+  }
 }
 
 data class TestVO(
-  val data1: String,
-  val data2: String
+  var data1: String = "",
+  var data2: String = ""
 )
