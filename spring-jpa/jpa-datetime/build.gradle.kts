d50fdf7fa78f7e824b4f0dc6c86be614122aebe2
daehyunKim <zbeld123@gmail.com>
2023-12-20 08:36:05 +0900

    chore(jpa): jpa datetime sample init

---
+++ spring-jpa/jpa-datetime/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.springframework.boot") version "3.2.0"
+  id("io.spring.dependency-management") version "1.1.4"
+  kotlin("jvm") version "1.9.20"
+  kotlin("plugin.spring") version "1.9.20"
+  kotlin("plugin.jpa") version "1.9.20"
+}
+
+allOpen {
+  annotation("jakarta.persistence.Entity")
+  annotation("jakarta.persistence.Embeddable")
+  annotation("jakarta.persistence.MappedSuperclass")
+}
+
+noArg {
+  annotation("jakarta.persistence.Entity")
+}
+
+group = "com.toy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs += "-Xjsr305=strict"
+    jvmTarget = "17"
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
---
+++ spring-jpa/jpa-datetime/src/main/kotlin/com/toy/jpadatetime/JpaDatetimeApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.jpadatetime
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaDatetimeApplication
+
+fun main(args: Array<String>) {
+  runApplication<JpaDatetimeApplication>(*args)
+}
