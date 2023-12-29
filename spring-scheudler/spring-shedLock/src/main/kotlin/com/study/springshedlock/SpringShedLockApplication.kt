0774a8bef029b51b6966574784d6a0f35abc51c7
daehyunKim <zbeld123@gmail.com>
2023-12-29 11:20:26 +0900

    chore(shedlock): sample project init

---
+++ spring-scheudler/spring-shedLock/src/main/kotlin/com/study/springshedlock/SpringShedLockApplication.kt
@@ -0,0 +1,11 @@
+package com.study.springshedlock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringShedLockApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringShedLockApplication>(*args)
+}
---
+++ spring-scheudler/spring-shedLock/src/test/kotlin/com/study/springshedlock/SpringShedLockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.springshedlock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringShedLockApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
