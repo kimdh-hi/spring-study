8efc48605e9408e34ede1f2128d5abc4f0cb0047
daehyunKim <zbeld123@gmail.com>
2023-12-30 13:50:03 +0900

    feat(boot3): jpa enum to varchar

--- spring-boot3/src/main/kotlin/com/toy/springboot3/domain/User.kt
+++ spring-boot3/src/main/kotlin/com/toy/springboot3/domain/User.kt
@@ -2,6 +2,8 @@ package com.toy.springboot3.domain
 
 import jakarta.persistence.*
 import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 
 @Table
 @Entity(name = "tb_user")
@@ -14,6 +16,7 @@ class User(
   var name: String?,
 
   @Enumerated(EnumType.STRING)
+  @JdbcTypeCode(SqlTypes.VARCHAR)
   @Column(name = "user_type")
   var userType: UserType
 )
