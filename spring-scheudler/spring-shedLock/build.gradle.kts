58be3de933f93e1b649e0dc1a34187466afd213e
daehyunKim <zbeld123@gmail.com>
2023-12-31 17:28:40 +0900

    chore(shedLock): dependencies

--- spring-scheudler/spring-shedLock/build.gradle.kts
+++ spring-scheudler/spring-shedLock/build.gradle.kts
@@ -20,6 +20,10 @@ repositories {
 }
 
 dependencies {
+  implementation("net.javacrumbs.shedlock:shedlock-spring:5.10.2")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.10.2")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-redis-spring:5.10.2")
+
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
