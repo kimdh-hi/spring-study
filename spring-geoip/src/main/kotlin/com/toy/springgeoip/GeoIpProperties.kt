e3bc6ab7751efe0bf25cfec6dd968b4bd891f6b4
daehyunKim <zbeld123@gmail.com>
2024-01-02 15:34:33 +0900

    chore: .

--- spring-geoip/src/main/kotlin/com/toy/springgeoip/GeoIpProperties.kt
+++
@@ -1,8 +0,0 @@
-package com.toy.springgeoip
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "geoip")
-class GeoIpProperties(
-  val path: String
-)
