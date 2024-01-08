d51ff78b94194c0f3b21af40fcd52176a6bc0a9d
daehyunKim <zbeld123@gmail.com>
2024-01-08 10:57:01 +0900

    feat(scheduler): async

--- spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/config/SchedulerConfig.kt
+++ spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/config/SchedulerConfig.kt
@@ -1,6 +1,7 @@
 package com.toy.springscheduler.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.SchedulingConfigurer
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
@@ -9,6 +10,7 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar
 
 @Configuration
 @EnableScheduling
+@EnableAsync
 class SchedulerConfig: SchedulingConfigurer {
 
   override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
--- spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/scheduler/ThreadPoolScheduler.kt
+++ spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/scheduler/ThreadPoolScheduler.kt
@@ -1,6 +1,7 @@
 package com.toy.springscheduler.scheduler
 
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -9,12 +10,14 @@ class ThreadPoolScheduler {
 
   private val log = LoggerFactory.getLogger(javaClass)
 
+  @Async
   @Scheduled(fixedDelay = 1000)
   fun scheduler1() {
     log.info("scheduler1...")
     Thread.sleep(10_000)
   }
 
+  @Async
   @Scheduled(fixedDelay = 1000)
   fun scheduler2() {
     log.info("scheduler2...")
