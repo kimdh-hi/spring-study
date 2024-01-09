8c42b166bc4d1c7c8528897648db0519b5fa66b4
daehyunKim <zbeld123@gmail.com>
2024-01-09 11:00:36 +0900

    feat(scheduler): cron tab

---
+++ spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/scheduler/CronTabScheduler.kt
@@ -0,0 +1,16 @@
+package com.toy.springscheduler.scheduler
+
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class CronTabScheduler {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  @Scheduled(cron = "0 0 11 * * *")
+  fun job() {
+    log.info("job...")
+  }
+}
--- spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/scheduler/ThreadPoolScheduler.kt
+++ spring-scheudler/spring-scheduler/src/main/kotlin/com/toy/springscheduler/scheduler/ThreadPoolScheduler.kt
@@ -5,19 +5,19 @@ import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
-@Component
+//@Component
 class ThreadPoolScheduler {
 
   private val log = LoggerFactory.getLogger(javaClass)
 
-  @Async
+//  @Async
   @Scheduled(fixedDelay = 1000)
   fun scheduler1() {
     log.info("scheduler1...")
     Thread.sleep(10_000)
   }
 
-  @Async
+//  @Async
   @Scheduled(fixedDelay = 1000)
   fun scheduler2() {
     log.info("scheduler2...")
