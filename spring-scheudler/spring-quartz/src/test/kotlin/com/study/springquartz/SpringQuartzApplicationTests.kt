0ccddc35bb32458e294159724158f97d125e9f60
daehyunKim <zbeld123@gmail.com>
2024-01-11 11:19:17 +0900

    chore(quartz): init

---
+++ spring-scheudler/spring-quartz/src/test/kotlin/com/study/springquartz/SpringQuartzApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.springquartz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringQuartzApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
