ae2ec5fec836c428951ead2e244f1a6643f08c82
daehyunKim <zbeld123@gmail.com>
2024-01-16 15:57:11 +0900

    chore(distributedTracing): init

---
+++ spring-cloud/spring-sleuth-zipkin/sleuthZipkinTestA/src/main/kotlin/com/sample/sleuthzipkintesta/SleuthZipkinTestAApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.sleuthzipkintesta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SleuthZipkinTestAApplication
+
+fun main(args: Array<String>) {
+  runApplication<SleuthZipkinTestAApplication>(*args)
+}
---
+++ spring-cloud/spring-sleuth-zipkin/sleuthZipkinTestA/src/test/kotlin/com/sample/sleuthzipkintesta/SleuthZipkinTestAApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sample.sleuthzipkintesta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SleuthZipkinTestAApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
