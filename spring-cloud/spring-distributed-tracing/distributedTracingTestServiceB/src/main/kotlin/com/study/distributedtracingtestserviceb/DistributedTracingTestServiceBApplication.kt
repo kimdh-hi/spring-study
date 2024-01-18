bf55623d4b45211795383aaa632d27130488ecd9
daehyunKim <zbeld123@gmail.com>
2024-01-18 10:06:05 +0900

    chore(distributedTracing): .

---
+++ spring-cloud/spring-distributed-tracing/distributedTracingTestServiceB/src/main/kotlin/com/study/distributedtracingtestserviceb/DistributedTracingTestServiceBApplication.kt
@@ -0,0 +1,11 @@
+package com.study.distributedtracingtestserviceb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DistributedTracingTestServiceBApplication
+
+fun main(args: Array<String>) {
+  runApplication<DistributedTracingTestServiceBApplication>(*args)
+}
---
+++ spring-cloud/spring-distributed-tracing/distributedTracingTestServiceB/src/test/kotlin/com/study/distributedtracingtestserviceb/DistributedTracingTestServiceBApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.distributedtracingtestserviceb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DistributedTracingTestServiceBApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
