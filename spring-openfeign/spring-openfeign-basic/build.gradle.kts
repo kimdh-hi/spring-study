41c60f54396ded32926207c56afa1ebdde8aaa93
daehyunKim <zbeld123@gmail.com>
2024-01-25 16:56:34 +0900

    refactor(micrometer-tracing): remove unnecessary code

--- spring-openfeign/spring-openfeign-basic/build.gradle.kts
+++ spring-openfeign/spring-openfeign-basic/build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.1.2"
-  id("io.spring.dependency-management") version "1.1.2"
-  kotlin("jvm") version "1.8.22"
-  kotlin("plugin.spring") version "1.8.22"
+  id("org.springframework.boot") version "3.2.2"
+  id("io.spring.dependency-management") version "1.1.4"
+  kotlin("jvm") version "1.9.22"
+  kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.toy"
 version = "0.0.1-SNAPSHOT"
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
+  extra["springCloudVersion"] = "2023.0.0"
 }
 
 repositories {
@@ -25,9 +25,10 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-//  implementation("io.github.openfeign:feign-httpclient")
+
 //  implementation("io.github.openfeign:feign-hc5")
   implementation("io.github.openfeign:feign-okhttp")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
