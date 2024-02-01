0f8e8884646421e729164fe41b9eb10a2eb65982
daehyunKim <zbeld123@gmail.com>
2024-02-01 10:54:04 +0900

    feat(batch): lecture project init

---
+++ spring-batch/spring-batch-lecture-inflearn/spring-batch/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.springframework.boot") version "3.2.2"
+  id("io.spring.dependency-management") version "1.1.4"
+  kotlin("jvm") version "1.9.22"
+  kotlin("plugin.spring") version "1.9.22"
+}
+
+group = "com.lecture"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-batch")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.batch:spring-batch-test")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs += "-Xjsr305=strict"
+    jvmTarget = "17"
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
---
+++ spring-batch/spring-batch-lecture-inflearn/spring-batch/src/main/kotlin/com/lecture/springbatch/SpringBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.springbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringBatchApplication>(*args)
+}
