7884259332dc977846fe3d81c3d524bc98021788
daehyunKim <zbeld123@gmail.com>
2024-03-23 09:31:17 +0900

    feat(restClient): loggingInterceptor

--- spring-http-client/spring-rest-client/src/main/kotlin/com/sample/springrestclient/config/RestClientConfig.kt
+++ spring-http-client/spring-rest-client/src/main/kotlin/com/sample/springrestclient/config/RestClientConfig.kt
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatusCode
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.client.ResponseErrorHandler
 import org.springframework.web.client.RestClient
@@ -14,13 +16,30 @@ import org.springframework.web.client.RestClient.ResponseSpec.ErrorHandler
 @Configuration
 class RestClientConfig {
 
-  private val log = LoggerFactory.getLogger(javaClass)
-
   @Bean
   fun restClient() = RestClient.builder()
     .defaultStatusHandler(RestClientErrorHandler())
+    .requestInterceptor(RestClientLoggingInterceptor())
     .build()
 }
+
+class RestClientLoggingInterceptor: ClientHttpRequestInterceptor {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  override fun intercept(
+    request: HttpRequest,
+    body: ByteArray,
+    execution: ClientHttpRequestExecution
+  ): ClientHttpResponse {
+    log.info("RestClient request: ${request.method} ${request.uri}")
+    val response = execution.execute(request, body)
+    log.info("RestClient response: statusCode = ${response.statusCode}")
+
+    return response
+  }
+}
+
 
 class RestClientErrorHandler: ResponseErrorHandler {
 
