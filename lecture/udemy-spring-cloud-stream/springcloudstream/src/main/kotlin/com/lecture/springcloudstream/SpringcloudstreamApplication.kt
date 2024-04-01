3106bb509fb7fe2b50f79e30ac8c0f2c4924188f
daehyunKim <zbeld123@gmail.com>
2024-04-01 10:02:45 +0900

    chore(cloudstream): lecture project init

---
+++ lecture/udemy-spring-cloud-stream/springcloudstream/src/main/kotlin/com/lecture/springcloudstream/SpringcloudstreamApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.springcloudstream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringcloudstreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringcloudstreamApplication>(*args)
+}
---
+++ lecture/udemy-spring-cloud-stream/springcloudstream/src/test/kotlin/com/lecture/springcloudstream/SpringcloudstreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.springcloudstream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringcloudstreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
