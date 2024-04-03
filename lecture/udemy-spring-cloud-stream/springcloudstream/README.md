3b635801c1fac10fcc00d6bae53fdf4b219d38bf
daehyunKim <zbeld123@gmail.com>
2024-04-03 10:22:48 +0900

    feat(cloudstream): sample consumer

---
+++ lecture/udemy-spring-cloud-stream/springcloudstream/README.md
@@ -0,0 +1,8 @@
+
+---
+
+```
+kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic input-topic --replication-factor 1 --partitions 1
+
+kafka-console-producer --bootstrap-server kafka:29092 --topic input-topic
+```
--- lecture/udemy-spring-cloud-stream/springcloudstream/src/main/kotlin/com/lecture/springcloudstream/sec01/KafkaConsumer.kt
+++ lecture/udemy-spring-cloud-stream/springcloudstream/src/main/kotlin/com/lecture/springcloudstream/sec01/KafkaConsumer.kt
@@ -1,4 +1,17 @@
 package com.lecture.springcloudstream.sec01
 
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.function.Consumer
+
+@Configuration
 class KafkaConsumer {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  @Bean
+  fun consumer(): Consumer<String> = Consumer {
+    log.info("consumer received: $it")
+  }
 }
