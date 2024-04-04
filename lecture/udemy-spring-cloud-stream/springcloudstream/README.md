a2f180e536d8cecd490f0b4470964dbb42fbaa7f
daehyunKim <zbeld123@gmail.com>
2024-04-04 14:02:30 +0900

    feat(cloudstream): function

--- lecture/udemy-spring-cloud-stream/springcloudstream/README.md
+++ lecture/udemy-spring-cloud-stream/springcloudstream/README.md
@@ -2,6 +2,8 @@
 ---
 
 ```
+docker exec -it kafka bash 
+
 kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic input-topic --replication-factor 1 --partitions 1
 
 kafka-console-producer --bootstrap-server kafka:29092 --topic input-topic
--- lecture/udemy-spring-cloud-stream/springcloudstream/src/main/kotlin/com/lecture/springcloudstream/sec01/KafkaConsumer.kt
+++ lecture/udemy-spring-cloud-stream/springcloudstream/src/main/kotlin/com/lecture/springcloudstream/sec01/KafkaConsumer.kt
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.function.Consumer
+import java.util.function.Function
 
 @Configuration
 class KafkaConsumer {
@@ -13,5 +14,10 @@ class KafkaConsumer {
   @Bean
   fun consumer(): Consumer<String> = Consumer {
     log.info("consumer received: $it")
+  }
+
+  @Bean
+  fun function() = Function<String, Unit> {
+    log.info("function received: $it")
   }
 }
