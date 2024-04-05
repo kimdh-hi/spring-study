c2de14710be9a3aa5bb792686eecd65a2b0593c3
daehyunKim <zbeld123@gmail.com>
2024-04-05 10:02:04 +0900

    chore(jpa):bulk insert sample init

---
+++ spring-jpa/jpa-bulk-insert/src/main/kotlin/com/toy/jpabulkinsert/JpaBulkInsertApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.jpabulkinsert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaBulkInsertApplication
+
+fun main(args: Array<String>) {
+  runApplication<JpaBulkInsertApplication>(*args)
+}
