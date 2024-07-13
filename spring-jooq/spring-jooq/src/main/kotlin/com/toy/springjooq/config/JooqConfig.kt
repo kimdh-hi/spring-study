57c34ea81cfe58adcf7f80700dd1862da8db80a8
daehyunKim <zbeld123@gmail.com>
2024-07-13 19:42:58 +0900

    feat(jOOQ): schema log disable

---
+++ spring-jooq/spring-jooq/src/main/kotlin/com/toy/springjooq/config/JooqConfig.kt
@@ -0,0 +1,12 @@
+package com.toy.springjooq.config
+
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JooqConfig {
+
+  fun customizer() = DefaultConfigurationCustomizer {
+    it.settings().withRenderSchema(false)
+  }
+}
