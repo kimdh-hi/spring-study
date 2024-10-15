4cd9e89fee1ba4064a2884e00df4b9a99f550ba0
daehyunKim <zbeld123@gmail.com>
2024-10-15 09:32:53 +0900

    chore(spring-event): init

---
+++ spring-event/src/main/kotlin/com/toy/springevent/SpringEventApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springevent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringEventApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringEventApplication>(*args)
+}
---
+++ spring-event/src/test/kotlin/com/toy/springevent/SpringEventApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springevent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringEventApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
