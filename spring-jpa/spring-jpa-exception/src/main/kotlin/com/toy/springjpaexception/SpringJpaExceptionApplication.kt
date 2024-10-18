e3130f0b948e1f322902f03786339042637734ef
daehyunKim <zbeld123@gmail.com>
2024-10-18 09:29:58 +0900

    chore(jpa): init

---
+++ spring-jpa/spring-jpa-exception/src/main/kotlin/com/toy/springjpaexception/SpringJpaExceptionApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springjpaexception
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaExceptionApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringJpaExceptionApplication>(*args)
+}
---
+++ spring-jpa/spring-jpa-exception/src/test/kotlin/com/toy/springjpaexception/SpringJpaExceptionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springjpaexception
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJpaExceptionApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
