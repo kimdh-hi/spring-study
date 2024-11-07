b9e522281cf8fcde54db88c213f455fe51c32608
daehyunKim <zbeld123@gmail.com>
2024-11-07 09:50:50 +0900

    chore(resilince4j):sample init

---
+++ spring-circuit-breaker/spring-resilience4j/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  id("org.springframework.boot") version "3.3.5"
+  id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.toy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+  implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+  imports {
+    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+  }
+}
+
+kotlin {
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
