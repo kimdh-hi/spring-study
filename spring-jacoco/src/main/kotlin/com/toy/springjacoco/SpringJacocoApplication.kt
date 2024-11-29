0a8f51f72334b0c361fc619cfb5a1ed0b127a92d
daehyunKim <zbeld123@gmail.com>
2024-11-29 18:07:59 +0900

    chore(jacoco):sample init

---
+++ spring-jacoco/src/main/kotlin/com/toy/springjacoco/SpringJacocoApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.springjacoco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJacocoApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringJacocoApplication>(*args)
+}
