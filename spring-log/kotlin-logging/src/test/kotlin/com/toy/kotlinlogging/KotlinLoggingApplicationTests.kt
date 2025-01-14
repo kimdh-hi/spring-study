6e2b12bcf3339cee3c83c2463b4d38e53c1b369e
daehyunKim <zbeld123@gmail.com>
2025-01-14 10:52:56 +0900

    chore(kotlin-logging): sample init

---
+++ spring-log/kotlin-logging/src/test/kotlin/com/toy/kotlinlogging/KotlinLoggingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.kotlinlogging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinLoggingApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
