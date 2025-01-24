de33e17cfd021b3cbd327b8cabc1b262060895c8
daehyunKim <zbeld123@gmail.com>
2025-01-24 12:02:31 +0900

    chore(exception): init

---
+++ spring-exception/src/test/kotlin/com/toy/springexception/SpringExceptionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.springexception
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringExceptionApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
