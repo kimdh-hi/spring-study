11945a048eb4681ddbee42f025ea31a477df917c
daehyunKim <zbeld123@gmail.com>
2025-02-19 16:49:15 +0900

    feat(kotlin-logging) .

--- spring-log/kotlin-logging/build.gradle.kts
+++ spring-log/kotlin-logging/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+  implementation("io.github.oshai:kotlin-logging-jvm:7.0.4")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- spring-log/kotlin-logging/src/main/kotlin/com/toy/kotlinlogging/service/LoggingService.kt
+++ spring-log/kotlin-logging/src/main/kotlin/com/toy/kotlinlogging/service/LoggingService.kt
@@ -4,6 +4,8 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
+//https://github.com/oshai/kotlin-logging/issues/364
+//top-level 선언 권장
 private val kotlinLogger = KotlinLogging.logger {}
 
 @Component
@@ -14,11 +16,17 @@ class LoggingService {
   private val innerKotlinLogger = KotlinLogging.logger {}
 
   fun stringTemplateLogging(data: String) {
+    val data = TestData("test")
     logger.debug("stringTemplateLogging data=$data")
+    logger.info("stringTemplateLogging data=$data")
+    logger.warn("stringTemplateLogging data=$data")
   }
 
   fun kotlinLogging(data: String) {
+    val data = TestData("test")
     kotlinLogger.debug { "kotlinLogging data=$data" } // c.t.k.service.LoggingServiceFile: kotlinLogging data=data
+    kotlinLogger.info { "kotlinLogging data=$data" }
+    kotlinLogger.warn { "kotlinLogging data=$data" }
     innerKotlinLogger.debug { "kotlinLogging data=$data" } // c.t.k.service.LoggingService: kotlinLogging data=data
   }
 
@@ -31,5 +39,13 @@ class LoggingService {
     }.onFailure {
       kotlinLogger.error(it) { "error.........." }
     }
+  }
+}
+
+data class TestData(
+  val data: String
+) {
+  override fun toString(): String {
+    return "TestData(data='$data')" // toString 호출 여부 확인
   }
 }
