4f194702cbc702fc7d06e9f45e110c12d29278c9
daehyunKim <zbeld123@gmail.com>
2025-03-25 13:40:12 +0900

    chore(spring-async): sample

---
+++ spring-async/src/main/kotlin/com/study/springasync/SpringAsyncApplication.kt
@@ -0,0 +1,11 @@
+package com.study.springasync
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAsyncApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAsyncApplication>(*args)
+}
---
+++ spring-async/src/test/kotlin/com/study/springasync/SpringAsyncApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.springasync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAsyncApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
