5a7318171fec45cb962bdc5e09f597edd2aa49ed
daehyunKim <zbeld123@gmail.com>
2025-04-24 10:14:50 +0900

    feat(spring-kotlin): general findByIdOrThrow repositry extension function

--- spring-kotlin/src/main/kotlin/com/toy/springkotlin/base/extensions/RepositoryExtensions.kt
+++ spring-kotlin/src/main/kotlin/com/toy/springkotlin/base/extensions/RepositoryExtensions.kt
@@ -2,8 +2,8 @@ package com.toy.springkotlin.base.extensions
 
 import org.springframework.data.repository.CrudRepository
 
-fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T? =
-  findById(id!!).orElse(null) ?: throw RuntimeException("data not found. id=$id")
+//fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T? =
+//  findById(id!!).orElse(null) ?: throw RuntimeException("data not found. id=$id")
 
-
-
+inline fun <reified T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T? =
+  findById(id!!).orElse(null) ?: throw RuntimeException("[Entity: ${T::class.simpleName}] data not found. id=$id")
