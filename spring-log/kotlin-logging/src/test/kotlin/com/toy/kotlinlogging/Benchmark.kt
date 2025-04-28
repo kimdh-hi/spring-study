49bb8f44f4dcb13a52f0ae7de8ac3e888d191ad0
daehyunKim <zbeld123@gmail.com>
2025-04-28 09:25:47 +0900

    feat(kotlin-logging): benchmark

--- spring-log/kotlin-logging/src/test/kotlin/com/toy/kotlinlogging/Benchmark.kt
+++ spring-log/kotlin-logging/src/test/kotlin/com/toy/kotlinlogging/Benchmark.kt
@@ -1,4 +1,63 @@
 package com.toy.kotlinlogging
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import kotlin.time.measureTime
+
+private val kotlinLogger = KotlinLogging.logger {}
+
 class Benchmark {
+
+  private val logger = LoggerFactory.getLogger(javaClass)
+
+  private val data1 = "data1"
+  private val data2 = "data2"
+  private val testCount = 100_000
+
+  @Test
+  fun parameterizedLogging() {
+    val times = mutableListOf<Long>()
+    repeat(5) {
+      val time = measureTime {
+        repeat(testCount) {
+          logger.debug("test data1={}, data2={}", data1, data2)
+        }
+      }.inWholeMilliseconds
+      times.add(time)
+    }
+
+    println("parameterizedLogging avg: ${times.average()}ms")
+  }
+
+  @Test
+  fun kotlinLogging() {
+    val times = mutableListOf<Long>()
+    repeat(10) {
+      val time = measureTime {
+        repeat(testCount) {
+          kotlinLogger.debug { "test data1=$data1 data2=$data2" }
+        }
+      }.inWholeMilliseconds
+      times.add(time)
+    }
+
+    println("kotlinLogging avg: ${times.average()}ms")
+  }
+
+  @Test
+  fun stringTemplateLogging() {
+    val times = mutableListOf<Long>()
+
+    repeat(10) {
+      val time = measureTime {
+        repeat(testCount) {
+          logger.debug("test data1=$data1 data2=$data2")
+        }
+      }.inWholeMilliseconds
+      times.add(time)
+    }
+
+    println("stringTemplateLogging avg: ${times.average()}ms")
+  }
 }
