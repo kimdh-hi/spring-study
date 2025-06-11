4ef5ecaedbb4e75eccc74fbf9fdefe30e008294f
daehyunKim <zbeld123@gmail.com>
2025-06-11 13:29:55 +0900

    chore(openfeign-querydsl): init

---
+++ openfeign-querydsl/src/main/kotlin/com/study/openfeignquerydsl/OpenfeignQuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.study.openfeignquerydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenfeignQuerydslApplication
+
+fun main(args: Array<String>) {
+  runApplication<OpenfeignQuerydslApplication>(*args)
+}
---
+++ openfeign-querydsl/src/test/kotlin/com/study/openfeignquerydsl/OpenfeignQuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.openfeignquerydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenfeignQuerydslApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
